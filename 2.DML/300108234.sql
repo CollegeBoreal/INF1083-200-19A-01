5e62b6d407ea2d0f15ee8b93352e052ae4a59d8f
Hali-Ma <43480203+halimabzn@users.noreply.github.com>
2019-09-26 15:02:55 -0400

    Update 300108234.sql

--- 2.DML/300108234.sql
+++ 2.DML/300108234.sql
@@ -1,1 +1,7 @@
--- Imprimer l'enregistrement comprenant le pays Algerie 
+--Imprimer l'enregistrement comprenant la ville Lubumbashi
+
+
+--Imprimer le nom et le montant de factures comprenant les habitant de Congo et l'algerie
+
+
+--Imprimer le nom et le montant de la facture totale des habitant de Congo et l'Algerie
