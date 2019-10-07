766e6d5a5ef91efc2e3b9f8c888a21277b5bff70
setrar <setrar@valiha.com>
2019-10-07 18:04:45 -0400

    .

--- 2.Templates/b300098957-blank-ng/src/app/app.component.ts
+++ 2.Templates/b300098957-blank-ng/src/app/app.component.ts
@@ -1,30 +1,7 @@
-import {Component, OnInit} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import { map, flatMap } from 'rxjs/operators';
+import { Component } from "@angular/core";
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html"
 })
-export class AppComponent implements OnInit {
-
-    pokemon: Array<any>;
-
-    constructor(private http: HttpClient) { }
-
-    ngOnInit(): void {
-        this.http.get("https://pokeapi.co/api/v2/pokemon?limit=151")
-            .pipe(
-            map( result => result ),
-            flatMap(result => result    )
-            )
-            .subscribe(
-                result => {
-                    this.pokemon.push(result);
-                },
-                error => {
-                    console.error(error);
-                }
-            );
-    }
-}
+export class AppComponent { }
