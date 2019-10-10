94dcf690832dc4d45ed179fcecc7f72863c1b3a1
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-10-10 14:43:42 -0400

    :car:

--- 3.DCL&DDL/300107361.sql
+++ 3.DCL&DDL/300107361.sql
@@ -4,9 +4,48 @@ CREATE DATABSE Car_Center;
 -- Mon utilisateur
 
 CREATE USER 'joker'@'localhost' IDENTIFIED BY 'etudiants_1';
-GRANT ALL ON etudiants.* TO 'etudiants'@'localhost';
+GRANT ALL ON joker.* TO 'joker'@'localhost';
 
+--selectionner la base de donnees 
+ 
+use Car_center
 
 -- Mes Tables
 
-CREATE TABLE 
+CREATE TABLE SERVICES_OFFERED (
+  name VARCHAR(30) NOT NULL
+  );
+  
+CREATE TABLE MARK (
+  name VARCHAR(30) NOT NULL
+  );
+  
+CREATE TABLE MODEL (
+   name VARCHAR(30) NOT NULL
+   );
+   
+CREATE TABLE ORIGIN (
+    name VARCHAR(30) NOT NULL
+    );
+    
+CREATE TABLE COLOUR (
+   name VARCHAR(30) NOT NULL
+   );
+   
+CREATE TABLE YEARS (
+  int VARCHAR(4) NOT NULL
+  );
+
+CREATE TABLE ENGIN_TYPE (
+  name VARCHAR(30) NOT NULL
+  );
+
+CREATE TABLE PRICE (
+  int VARCHAR(8) NOT NULL
+  );
+
+CREATE TABLE MAINTENANCE (
+  name VARCHAR(30) NOT NULL
+  );
+  
+  
