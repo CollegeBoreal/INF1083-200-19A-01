4910b55bb1cd0c1a9a5204eeb914689eac73eac7
setrar <setrar@valiha.com>
2019-10-10 14:15:36 -0400

    Update 300098957.sql

--- 3.DCL&DDL/300098957.sql
+++ 3.DCL&DDL/300098957.sql
@@ -6,16 +6,19 @@ CREATE DATABASE etudiants;
 CREATE USER 'etudiants'@'localhost' IDENTIFIED BY 'etudiants_1';
 GRANT ALL ON etudiants.* TO 'etudiants'@'localhost';
 
+-- selectionner la base de donnees
 use etudiants;
 
 -- Mes Tables
 
 CREATE TABLE ETUDIANTS (
-  nom VARCHAR(30) NOT NULL
+  initiale VARCHAR(30) NOT NULL
   );
 
 CREATE TABLE NOTES (
   nom VARCHAR(30) NOT NULL,
   note INT
   );
+  
+  -- A vous de jouer
 
