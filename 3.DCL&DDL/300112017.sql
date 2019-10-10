56532445bedfed1198862e8cb4f5a401699c9789
ordenrosae <43898171+ordenrosae@users.noreply.github.com>
2019-10-10 15:42:54 -0400

    Create 300112017.sql

---
+++ 3.DCL&DDL/300112017.sql
@@ -0,0 +1,19 @@
+Mon domaine
+                                CREATE DATABASE magasin
+
+-- Mon utilisateur
+
+
+CREATE USER 'magasin'@'localhost' IDENTIFIED BY 'etudiants_1';
+GRANT ALL ON magasin.* TO 'magasin'@'localhost';
+
+-- selectionner la base de donnees
+use magasin;
+
+
+-- Mes Tables
+CREATE TABLE clients(
+ num_client INT NOT NULL,
+     nom VARCHAR(30),
+    prenom VARCHAR(20)
+    );
