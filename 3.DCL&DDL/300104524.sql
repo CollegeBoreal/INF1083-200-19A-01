7efa6753eea0f611d2ac36ea1f1b350b18a79cd1
Echnaideurgeneus <43045076+Echnaideurgeneus@users.noreply.github.com>
2019-10-10 12:21:21 -0700

    Update 300104524.sql

--- 3.DCL&DDL/300104524.sql
+++ 3.DCL&DDL/300104524.sql
@@ -3,11 +3,11 @@ CREATE DATABASE technical_support;
 CREATE USER 'technical_support'@'localhost' IDENTIFIED BY 'technical_support_1';
 GRANT ALL ON technical_support.* TO 'technical_support'@'localhost';
 
-use technical_support
+use technical_support;
 
 CREATE TABLE REPAIRS (nom VARCHAR(30) NOT NULL, brand VARCHAR(30) NOT NULL);
 
 CREATE TABLE SERVICES (BRAND VARCHAR(30) NOT NULL, problem VARCHAR(30) NOT NULL);
 
-CREATE TABLE PAYMENT (nom VARCHAR(30) NOT NULL, note INT);
+CREATE TABLE PAYMENTS (nom VARCHAR(30) NOT NULL, note INT);
 
