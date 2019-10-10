15c6cc6afe99797588331dc615712965341f7351
setrar <setrar@valiha.com>
2019-10-10 13:42:04 -0400

    Update 300098957.sql

--- 3.DCL&DDL/300098957.sql
+++ 3.DCL&DDL/300098957.sql
@@ -3,3 +3,11 @@ CREATE DATABSE etudiants;
 
 -- Mon utilisateur
 
+CREATE USER 'etudiants'@'localhost' IDENTIFIED BY 'etudiants_1';
+GRANT ALL ON etudiants.* TO 'etudiants'@'localhost';
+
+
+-- Mes Tables
+
+CREATE TABLE .......
+
