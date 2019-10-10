657ddd712a756bd5cbf9ec9d9a00545bf65e9712
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-10-10 14:08:47 -0400

    :car:

--- 3.DCL&DDL/300107361.sql
+++ 3.DCL&DDL/300107361.sql
@@ -3,7 +3,7 @@ CREATE DATABSE Car_Center;
 
 -- Mon utilisateur
 
-CREATE USER 'etudiants'@'localhost' IDENTIFIED BY 'etudiants_1';
+CREATE USER 'joker'@'localhost' IDENTIFIED BY 'etudiants_1';
 GRANT ALL ON etudiants.* TO 'etudiants'@'localhost';
 
 
