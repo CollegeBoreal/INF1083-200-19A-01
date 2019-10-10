15acdc24b61279b4700f3d3b05bbb2da0dd098e6
widby <43479739+widby@users.noreply.github.com>
2019-10-10 14:35:56 -0400

    Update 300113775.sql

--- 3.DCL&DDL/300113775.sql
+++ 3.DCL&DDL/300113775.sql
@@ -1,1 +1,36 @@
--- 
+-- Mon Domaine
+CREATE DATABASA Picasa;
+
+
+
+--Mon Utilisateur
+CREATE USER 'Picasa'@'localhost' IDENTIFIED BY 'Picasa_1';
+GRANT ALL ON Picasa.* TO 'Picasa'@'localhost';
+
+
+
+--Selectionner la base de donnée
+use Picasa;
+
+
+--mes tables 
+CREATE TABLE MODES (
+  initiale VARCHAR(15) NOT NULL
+  );
+  
+ CREATE TABLE COULEURS (
+     initiale VARCHAR(10) NOT NULL
+     );
+     
+ CREATE TABLE FORMATS (
+     initiale VARCHAR(5) NOT NULL
+     );
+  
+  
+ CREATE TABLE SOCIAL MEDIAS (
+     initiale VARCHAR(10) NOT NULL
+     );
+     
+   CREATE TABLE FRAMES (
+     initiale VARCHAR(10) NOT NULL
+     );
