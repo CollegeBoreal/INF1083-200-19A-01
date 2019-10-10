df6aba7aa96011721990202e1447abc809d8dbee
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2019-10-10 15:48:16 -0400

    Update 300106918.sql

--- 3.DCL&DDL/300106918.sql
+++ 3.DCL&DDL/300106918.sql
@@ -1,1 +1,24 @@
 
+ Mon domaine
+
+CREATE DATABASE network_IT;
+
+-- Mon utilisateur
+
+CREATE USER 'AEK'@'localhost' IDENTIFIED BY 'AEK';
+
+GRANT ALL ON network_IT.* TO 'AEK'@'localhost';
+
+-- selectionner la base de donnees
+
+use network_IT;
+
+-- Mes Tables
+
+CREATE TABLE SOLUTION ( Nom VARCHAR(30) NOT NULL );
+  
+CREATE TABLE PRODUCTS ( Nom VARCHAR(30) NOT NULL, Date INT );
+  
+  CREATE TABLE INSTALLATION ( Nom VARCHAR(30) NOT NULL );
+  
+  CREATE TABLE PRIX (Nom VARCHAR(30) NOT NULL, Tarif INT);
