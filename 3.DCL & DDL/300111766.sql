7c7d5fa2d6f335789fc0f9dde03f191d9438e730
cheikthiam <44683625+Cheikhthiam@users.noreply.github.com>
2019-10-12 20:31:44 -0700

    Create 300111766.sql

---
+++ 3.DCL & DDL/300111766.sql
@@ -0,0 +1,61 @@
+	CREATE DATABESES
+Vente_portable
+
+
+	
+
+	
+	-- Mon utilisateur
+
+
+CREATE USER 'vente_prtable'@'localhost' IDENTIFIED BY 'password'
+GRANT ALL ON Vente_portable* TO      'Vente_portable'@'localhost';
+
+
+	
+	
+	
+
+	-- selectionner la base de donnees
+use telephones;
+
+	
+	
+
+	-- Mes Tables
+	
+
+	CREATE TABLE telephones(
+	  initiale VARCHAR(30) NOT NULL
+	  );
+	
+
+	
+	
+	
+
+	
+
+	CREATE TABLE telephones 
+(
+	
+NON_MARQUE INT NOT NUL,
+NUMERO_SERIE VAR(30) NOT NUL,
+PRIX INT
+);
+CREATE TABLE Accessoires
+)
+Chargeur VARCHAR(30) NOT NULL,
+Ecran VARCHAR(30) NOT NULL,
+Protége VARCHAR(30) N0T NULL,
+Prix INT
+);--
+
+	
+	
+
+	
+	 
+	
+	
+
