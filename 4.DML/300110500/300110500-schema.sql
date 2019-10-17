12a72bd5bf2eb4865e7f7e09e4737e82bcfca90d
didier300110500 <guigma_d@yahoo.fr>
2019-10-17 16:20:30 -0400

    je suis la

---
+++ 4.DML/300110500/300110500-schema.sql
@@ -0,0 +1,35 @@
+-- Mon domaine
+
+CREATE DATABASE immigration;
+
+-- Mon utilsateur
+
+CREATE USER 'guigma'@'localhost' IDENTIFIED BY 'guigma_1';
+GRANT ALL ON immigration.* TO 'guigma'@'localhost';
+
+-- selectionner la base de donnees
+use immigration;
+
+-- Mes Tables
+
+CREATE TABLE Visas (
+  initiale VARCHAR(35) NOT NULL
+  );
+  
+  CREATE TABLE Payments (
+  initiale VARCHAR(35) NOT NULL
+  );
+
+CREATE TABLE Countries1 (
+  initiale VARCHAR(35) NOT NULL
+  );
+  
+  CREATE TABLE Counties2 (
+  initiale VARCHAR(35) NOT NULL
+  );
+  
+  
+  CREATE TABLE Services (
+  initiale VARCHAR(35) NOT NULL
+  );
+
