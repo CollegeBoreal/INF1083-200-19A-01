bdfa5bdacbb022d8594722ad6103054640309949
Etienne Tochgaly <etiennetochgaly@gmail.com>
2019-10-16 20:13:38 -0400

    .

---
+++ 2.Templates/b300107361/src/app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./home.component";
+import { ItemDetailComponent } from "./item-detail/item-detail.component";
+
+@NgModule({
+    imports: [
+        NativeScriptCommonModule,
+        HomeRoutingModule
+    ],
+    declarations: [
+        HomeComponent,
+        ItemDetailComponent
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class HomeModule { }
