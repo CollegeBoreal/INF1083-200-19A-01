484a16c707f76d1e3c55a581199fb7c805145286
setrar <setrar@valiha.com>
2019-10-17 15:25:56 -0400

    Update 300098957-data.sql

---
+++ 4.DML/300098957/300098957-data.sql
@@ -0,0 +1,6 @@
+
+
+-- Données de la table étudiants
+
+INSERT INTO ETUDIANTS ( initiale, nom, age ) VALUES ( 'BR', 'Robert', 55);
+INSERT INTO ETUDIANTS ( initiale, nom, age ) VALUES ( 'CT', 'Thiam', 33);
