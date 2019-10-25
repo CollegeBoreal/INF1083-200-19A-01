c47de4d53b4e071c265d78053be4d8db0cbec8f0
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-10-24 21:47:25 -0400

    hm!

---
+++ 4.DML/300107361/300107361-data.sql
@@ -0,0 +1,15 @@
+-- Dans le domaine
+
+use etudiants;
+
+-- Données de la table étudiants
+
+INSERT INTO SERVICES_OFFERED (Sale, Insurance, Cleaning  ) VALUES ( 'Car_Trucks', 'New_Driver', 'Car_Wash');
+INSERT INTO MARKS ( Mark ) VALUES ('Acura' , 'Audi', 'Bentley' , 'BMW' , 'Buick' , 'Cadilac' , 'Chevrolet' , 'Chrysler' , 'CODA' , 'Daihatsu' , 'Dodge' , 'Ferrari' , 'FIAT' , 'Ford' , 'GMC' , 'Honda' , 'Hyundai' , 'Infiniti' , 'Izuzu' , 'Jaguar' , 'Jeep' , 'Kia' , 'Lamborghini' , 'Land_Rover' , 'Lexus' , 'LOtus' , 'Maserati' , 'Maybach' , 'Mazda' , 'Mercedes_Benz' , 'Mitsubishi' , 'Nissan' , 'Porsche' , 'Subaru' , 'Suzuki' , 'Toyota' , 'Volkswagen' , 'Volvo_48' , 'Yamaha' );
+INSERT INTO MODELS (Model) VALUES ('CL', 'CSX', 'EL', 'ILX', 'INTEGRA', 'LEGEND', 'VIGOR', 100, 200, 'CABRIOLET', 'R8' 'GT_SPYDER', '1_SERIES', '640' 'GRAN_COUP', '640_GRAN_TURISMO', 'AVALANCHE', 'AVEO', 'IMPALA', 'BERETA', 'G30', '328', '348_SPYDER', 'CHEROKEE', 'WRANGLER', 'GLADIATOR', 'COMPASS', 'COMMANDER');
+INSERT INTO ORIGINS (Origins) VALUES ('USA', 'Japan', 'Germany', 'Italy',);
+INSERT INTO COLOURS (Colours) VALUES ('Black', 'Bleue', 'Gray', 'white', 'green', 'red', 'yellow', '...');
+INSERT INTO YEARS (Years) VALUES (1990_2000, 2000_2010, 2010_2020);
+INSERT INTO ENGIN_TYPES (Engins) VALUES ('Electric', 'Hybrid', 'petrol', 'Suv', 'truck', 'regular', 'sport_car');
+INSERT INTO PRICES (Prices) VALUES (18000-400000);
+INSERT INTO MAINTENANCES (Maintenances) VALUES ('tires_repair', 'transmission', 'brake');
