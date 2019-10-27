db564d3961a83f6f2307c684b5bd1b3484fc639a
setrar <setrar@valiha.com>
2019-10-26 22:05:43 -0400

    :fire:

--- 2.Templates/b300098957-blank-ng/src/app/home/home.component.ts
+++ 2.Templates/b300098957-blank-ng/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, ElementRef, OnInit} from "@angular/core";
 
 @Component({
     selector: "Home",
@@ -6,7 +6,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
 
-    constructor() {
+    constructor(public elementRef: ElementRef) {
         // Use the component constructor to inject providers.
     }
 
