fe49072db7e80066a676ed92bc6ed581d78e4d26
didier300110500 <43076393+didier300110500@users.noreply.github.com>
2019-10-29 14:33:11 -0400

    Update 300110500-schema.sql

--- 4.DML/300110500/300110500-schema.sql
+++ 4.DML/300110500/300110500-schema.sql
@@ -23,8 +23,8 @@ use immigration_consulting;
   CREATE TABLE Honoraire_payments (
   Name VARCHAR(30) NOT NULL 
   Especes INT AUTO_INCREMENT,
-  Carte_bancaire INT NOT NULL,
-  Western_union INT NOT NULL,
+  Carte_bancaire INT,
+  Western_union INT,
   PRIMARY KEY(Especes)
   );  
                                                                                                                                                                                                                       
