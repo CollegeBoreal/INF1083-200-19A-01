e9e3b424500ce1c557ad519713df5de12ab2f63f
setrar <setrar@valiha.com>
2019-10-29 11:28:13 -0400

    .

---
+++ 4.DML/notation.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# --------------------------------------
+#
+#  Correction 4.DML
+#
+#
+# --------------------------------------
+
+ETUDIANTS=(
+ 300098957
+ 300104524
+ 300104541
+ 300105201
+ 300106918
+ 300107361
+ 300108234
+ 300110500
+ 300110529
+ 300111671
+ 300111766
+ 300112017
+ 300112687
+ 300112917
+ 300113775
+)
+
+for id in "${ETUDIANTS[@]}"
+do
+   echo " =[BEGIN]============== Exécution du script ${id} =============== "
+
+   # Charger le schema
+
+   docker exec --interactive some-mysql \
+      mysql --user root -ppassword < ~/Developer/INF1006-202-19A-01/4.DML/${id}/${id}-schema.sql
+
+   # Charger les données
+
+   docker exec --interactive some-mysql \
+      mysql --user root -ppassword < ~/Developer/INF1006-202-19A-01/4.DML/${id}/${id}-data.sql
+
+   echo " =[END]============== Exécution du script ${id} =============== "
+done
