d684828f21bce4627dbd6d257dad399784b14bb7
widby <43479739+widby@users.noreply.github.com>
2019-10-29 14:41:12 -0400

    Update 300113775-data.sql

--- 4.DML/300113775/300113775-data.sql
+++ 4.DML/300113775/300113775-data.sql
@@ -6,5 +6,5 @@ use picasa;
 
 -- Données de la table picasa
 
-INSERT INTO PICASA ( modes, format, reseaux_sociaux) VALUES ( 'portrait', 'RGB', 'Facebook');
-INSERT INTO PICASA ( modes, format, reseaux_sociaux) VALUES ('paysage', 'CMYK', 'Instagram');
+INSERT INTO picasa ( modes, format, reseaux_sociaux) VALUES ( 'portrait', 'RGB', 'Facebook');
+INSERT INTO picasa ( modes, format, reseaux_sociaux) VALUES ('paysage', 'CMYK', 'Instagram');
