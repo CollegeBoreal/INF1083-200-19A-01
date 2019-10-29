5b5a875bc0ca58ae780c95d4563638d4d30442c4
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2019-10-29 14:18:36 -0400

    Update 300106918-schema.sql

--- 4.DML/300106918/300106918-schema.sql
+++ 4.DML/300106918/300106918-schema.sql
@@ -15,13 +15,13 @@ use network_IT;
 
 -- Mes Tables
 
-CREATE TABLE SOLUTION ( solution INT AUTO_INCREMENT, 
+CREATE TABLE SOLUTIONS ( solution INT AUTO_INCREMENT, 
                        Nom VARCHAR(30) NOT NULL,
                        PRIMARY KEY(solution));
   
 CREATE TABLE PRODUCTS ( Nom VARCHAR(30) NOT NULL, Date INT);
                      
-CREATE TABLE INSTALLATION (installation INT AUTO_INCREMENT,
+CREATE TABLE INSTALLATIONS (installation INT AUTO_INCREMENT,
                            Nom VARCHAR(30) NOT NULL,
                            PRIMARY KEY(installation));
   
@@ -31,6 +31,6 @@ CREATE TABLE PRIX (Nom VARCHAR(30) NOT NULL, Tarif INT,
                    prix INT,
             PRIMARY KEY( solution,installation),
             FOREIGN KEY(solution) 
-            REFERENCES SOLUTION(solution),
+            REFERENCES SOLUTIONS(solution),
             FOREIGN KEY(installation) 
-            REFERENCES INSTALLATION(installation));
+            REFERENCES INSTALLATIONS(installation));
