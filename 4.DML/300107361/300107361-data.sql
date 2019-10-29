0b27722706d2f2e6d315315c5829d6a44740741f
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-10-29 13:35:02 -0400

    :car:

--- 4.DML/300107361/300107361-data.sql
+++ 4.DML/300107361/300107361-data.sql
@@ -5,8 +5,8 @@ use Car_Center;
 -- Données des tables
 
 INSERT INTO SERVICES_OFFERED (Sale) VALUES ( 'Car_Trucks');
-INSERT INTO MARKS (Mark) VALUES ('Acura' , 'Audi', 'BMW' ,'Ferrari' , 'Ford' , 'GMC' , 'Honda' , 'Hyundai' , 'Infiniti' ,'Jaguar', 'Jeep','Lamborghini', 'Lexus', 'Nissan' , 'Porsche' , 'Toyota');
-INSERT INTO MODELS (Model) VALUES ('CL', 'LEGEND', 'CABRIOLET', 'GT_SPYDER', 'AVALANCHE', 'AVEO', 'IMPALA', 'CHEROKEE', 'WRANGLER');
+INSERT INTO MARKS (Marks) VALUES ('Acura' , 'Audi', 'BMW' ,'Ferrari' , 'Ford' , 'GMC' , 'Honda' , 'Hyundai' , 'Infiniti' ,'Jaguar', 'Jeep','Lamborghini', 'Lexus', 'Nissan' , 'Porsche' , 'Toyota');
+INSERT INTO MODELS (Models) VALUES ('CL', 'LEGEND', 'CABRIOLET', 'GT_SPYDER', 'AVALANCHE', 'AVEO', 'IMPALA', 'CHEROKEE', 'WRANGLER');
 INSERT INTO ORIGINS (Origins) VALUES ('USA', 'Japan', 'Germany', 'Italy',);
 INSERT INTO COLOURS (Colours) VALUES ('Black', 'Bleue', 'Gray', 'white', 'green', 'red', 'yellow', '...');
 INSERT INTO YEARS (Years) VALUES (1990_2000, 2000_2010, 2010_2020);
