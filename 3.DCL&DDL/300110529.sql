90d483ba1b360d4ad9ecaebe0a59f99e5c52dd5f
David <36050681+TEC24@users.noreply.github.com>
2019-10-29 09:12:46 -0700

    Update 300110529.sql

--- 3.DCL&DDL/300110529.sql
+++ 3.DCL&DDL/300110529.sql
@@ -14,83 +14,26 @@ use Carte_graphique;
 
 
 -- Mes Tables
-
-
-CREATE TABLE Marques (
-
-  NAME VARCHAR(30) NOT NULL,
-  Marques INT
-
-  );
-  
-   CREATE TABLE clients(
-            num_client INT NOT NULL,
-	    nom_client VARCHAR(30),
-	    prenom_client VARCHAR(20)
+	 CREATE TABLE clients(
+num_client INT NOT NULL,
+	     nom VARCHAR(30),
+	    prenom VARCHAR(20)
 	    );
-  
-  CREATE TABLE marchandises (
-	    num_marchandise INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	    nom_marchandise VARCHAR(20),
-	    prix_marchandise INT(20) NOT NULL,
-	    description_marchandise TEXT
-               );
-CREATE TABLE CAPACITE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE  MODELE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE COULEUR (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE DELAI (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE QUANTITE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE DUREE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE ANNEE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE GARANTIE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE PRIX (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE PAIEMENT (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
+	CREATE TABLE marchandises(
+	       num_marchandise INT NOT NULL,
+	       nom_marchandise VARCHAR(20),
+	       prix_marchandise INT(20) NOT NULL,
+	       description_marchandise TEXT
+	      );
+	CREATE TABLE PAYMENTS(
+	          nom VARCHAR(30) NOT NULL, 
+	          note INT
+	);
+	CREATE TABLE Capacites(
+	          nom VARCHAR(30) NOT NULL, 
+	          note INT
+	);
+	CREATE TABLE Modeles(
+	          nom VARCHAR(30) NOT NULL, 
+	          note INT
+	 ); 
