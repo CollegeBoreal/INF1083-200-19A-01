520bd28710f7bc746fc4fd65d480430173ca4c89
setrar <setrar@valiha.com>
2019-10-29 11:05:08 -0400

    Update 300098957-data.sql

--- 4.DML/300098957/300098957-data.sql
+++ 4.DML/300098957/300098957-data.sql
@@ -6,3 +6,11 @@ use etudiants;
 
 INSERT INTO ETUDIANTS ( initiale, nom, age ) VALUES ( 'BR', 'Robert', 55);
 INSERT INTO ETUDIANTS ( initiale, nom, age ) VALUES ( 'CT', 'Thiam', 33);
+
+-- Données de la table devoirs
+
+INSERT INTO DEVOIRS ( nom, evaluation ) VALUES ( 'Installation Linux', '2019-10-03');
+INSERT INTO DEVOIRS ( nom, evaluation ) VALUES ( 'Tout sur Nativescript', '2019-09-03');
+INSERT INTO DEVOIRS ( nom, evaluation ) VALUES ( 'Pourquoi Git?', '2019-11-03');
+INSERT INTO DEVOIRS ( nom, evaluation ) VALUES ( 'Que faire après le collège?', '2020-04-18');
+
