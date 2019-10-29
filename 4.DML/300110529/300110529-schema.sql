84ede959c13086b9dd68c6333c13bd2443d1bca9
David <36050681+TEC24@users.noreply.github.com>
2019-10-29 08:43:57 -0700

    Update 300110529-schema.sql

--- 4.DML/300110529/300110529-schema.sql
+++ 4.DML/300110529/300110529-schema.sql
@@ -2,16 +2,10 @@
 
 CREATE DATABASE Carte_graphique;
 
-
-
 -- Mon utilisateur
 
-
-
 CREATE USER 'Carte_graphique'@'localhost' IDENTIFIED BY 'Carte_graphique';
-
 GRANT ALL ON Carte_graphique.* TO 'Carte_graphique'@'localhost';
-
 
 
 -- selectionner la base de donnees
@@ -19,17 +13,28 @@ GRANT ALL ON Carte_graphique.* TO 'Carte_graphique'@'localhost';
 use Carte_graphique;
 
 
-
 -- Mes Tables
 
 
+CREATE TABLE Marques (
 
-CREATE TABLE MARQUE (
-
-  NAME VARCHAR(30) NOT NULL
+  NAME VARCHAR(30) NOT NULL,
+  Marques INT
 
   );
-
+  
+   CREATE TABLE clients(
+            num_client INT NOT NULL,
+	    nom_client VARCHAR(30),
+	    prenom_client VARCHAR(20)
+	    );
+  
+  CREATE TABLE marchandises (
+	    num_marchandise INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	    nom_marchandise VARCHAR(20),
+	    prix_marchandise INT(20) NOT NULL,
+	    description_marchandise TEXT
+               );
 CREATE TABLE CAPACITE (
 
   NAME VARCHAR(30) NOT NULL
@@ -89,10 +94,3 @@ CREATE TABLE PAIEMENT (
   NAME VARCHAR(30) NOT NULL
 
   );
-
-
-
-
-
-
-
