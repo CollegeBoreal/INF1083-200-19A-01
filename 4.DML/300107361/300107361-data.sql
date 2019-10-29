57828f6facf3d15eb742f4083e81d318cf94b40e
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-10-29 13:12:44 -0400

    Update 300107361-data.sql

--- 4.DML/300107361/300107361-data.sql
+++ 4.DML/300107361/300107361-data.sql
@@ -5,10 +5,8 @@ use Car_Center;
 -- Données des tables
 
 INSERT INTO SERVICES_OFFERED (Sale) VALUES ( 'Car_Trucks');
-INSERT INTO SERVICES_OFFERED (Insurance) VALUES ( 'New_Driver');
-INSERT INTO SERVICES_OFFERED (Cleaning) VALUES ( 'Car_Wash');
-INSERT INTO MARKS ( Mark ) VALUES ('Acura' , 'Audi', 'Bentley' , 'BMW' , 'Buick' , 'Cadilac' , 'Chevrolet' , 'Chrysler' , 'CODA' , 'Daihatsu' , 'Dodge' , 'Ferrari' , 'FIAT' , 'Ford' , 'GMC' , 'Honda' , 'Hyundai' , 'Infiniti' , 'Izuzu' , 'Jaguar' , 'Jeep' , 'Kia' , 'Lamborghini' , 'Land_Rover' , 'Lexus' , 'LOtus' , 'Maserati' , 'Maybach' , 'Mazda' , 'Mercedes_Benz' , 'Mitsubishi' , 'Nissan' , 'Porsche' , 'Subaru' , 'Suzuki' , 'Toyota' , 'Volkswagen' , 'Volvo_48' , 'Yamaha' );
-INSERT INTO MODELS (Model) VALUES ('CL', 'CSX', 'EL', 'ILX', 'INTEGRA', 'LEGEND', 'VIGOR', 100, 200, 'CABRIOLET', 'R8' 'GT_SPYDER', '1_SERIES', '640' 'GRAN_COUP', '640_GRAN_TURISMO', 'AVALANCHE', 'AVEO', 'IMPALA', 'BERETA', 'G30', '328', '348_SPYDER', 'CHEROKEE', 'WRANGLER', 'GLADIATOR', 'COMPASS', 'COMMANDER');
+INSERT INTO MARKS ( Mark ) VALUES ('Acura' , 'Audi', 'BMW' ,'Ferrari' , 'Ford' , 'GMC' , 'Honda' , 'Hyundai' , 'Infiniti' ,'Jaguar', 'Jeep','Lamborghini', 'Lexus', 'Nissan' , 'Porsche' , 'Toyota');
+INSERT INTO MODELS (Model) VALUES ('CL', 'LEGEND', 'CABRIOLET', 'GT_SPYDER', 'AVALANCHE', 'AVEO', 'IMPALA', 'CHEROKEE', 'WRANGLER');
 INSERT INTO ORIGINS (Origins) VALUES ('USA', 'Japan', 'Germany', 'Italy',);
 INSERT INTO COLOURS (Colours) VALUES ('Black', 'Bleue', 'Gray', 'white', 'green', 'red', 'yellow', '...');
 INSERT INTO YEARS (Years) VALUES (1990_2000, 2000_2010, 2010_2020);
