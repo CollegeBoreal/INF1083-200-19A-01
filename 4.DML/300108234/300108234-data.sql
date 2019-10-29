ad9ec3baa0f383578e1417440be4f1b1c2d88e0c
Hali-Ma <43480203+halimabzn@users.noreply.github.com>
2019-10-29 15:42:15 -0400

    🤷‍♀️

--- 4.DML/300108234/300108234-data.sql
+++ 4.DML/300108234/300108234-data.sql
@@ -1,1 +1,28 @@
+-- Dans le domaine
+
+use Hijabfashion;
+
+-- Données de la table couleurs
+
+INSERT INTO Couleurs ( nom ) VALUES ( 'Noir');
+
+-- Données de la table prix
+
+INSERT INTO Prix ( nom, tarif ) VALUES ( 10, 13%);
+
+-- Données de la table tailles
+
+INSERT INTO Tailles ( Largeur, Longeur, Unite_de_mesure ) VALUES ( 55, 160, 'cm');
+
+-- Données de la table tissu
+
+INSERT INTO Tissu ( nom) VALUES ( 'Cotton');
+
+-- Données de la table accessoires
+
+INSERT INTO Accessoires ( nom, type ) VALUES ( 'Perle','Coloree');
+
+-- Données de la table pays de fabrication
+
+INSERT INTO Pays_de_fabrication ( nom, devise) VALUES ( 'Maroc', 'Dirham');
 
