9bcf3c1946a13c637c1c53778b800fedbd13524a
David <36050681+TEC24@users.noreply.github.com>
2019-10-29 09:28:07 -0700

    Update 300110529-schema.sql

--- 4.DML/300110529/300110529-schema.sql
+++ 4.DML/300110529/300110529-schema.sql
@@ -15,82 +15,35 @@ use Carte_graphique;
 
 -- Mes Tables
 
-
-CREATE TABLE Marques (
-
-  NAME VARCHAR(30) NOT NULL,
-  Marques INT
-
-  );
-  
-   CREATE TABLE clients(
-            num_client INT NOT NULL,
-	    nom_client VARCHAR(30),
-	    prenom_client VARCHAR(20)
-	    );
-  
-  CREATE TABLE marchandises (
-	    num_marchandise INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	    nom_marchandise VARCHAR(20),
-	    prix_marchandise INT(20) NOT NULL,
-	    description_marchandise TEXT
-               );
-CREATE TABLE CAPACITE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE  MODELE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE COULEUR (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE DELAI (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE QUANTITE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE DUREE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE ANNEE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE GARANTIE (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE PRIX (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
-
-CREATE TABLE PAIEMENT (
-
-  NAME VARCHAR(30) NOT NULL
-
-  );
+		 CREATE TABLE clients(
+	num_client INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		     nom VARCHAR(30),
+		    prenom VARCHAR(20)
+		    );
+		CREATE TABLE marchandises(
+		       num_marchandise INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		       nom_marchandise VARCHAR(20),
+		       prix_marchandise INT(20) NOT NULL,
+		       description_marchandise TEXT
+		      );
+		CREATE TABLE PAYMENTS(
+		          nom VARCHAR(30) NOT NULL, 
+		          note INT
+		);
+		CREATE TABLE Capacites(
+		          nom VARCHAR(30) NOT NULL, 
+		          note INT
+		);
+		CREATE TABLE Modeles(
+		          nom VARCHAR(30) NOT NULL, 
+		          note INT
+		 ); 
+	
+                  CREATE TABLE ventes(
+      num_vente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      date_vente DATE,
+      marchandise_num INT,
+      client_num INT,
+       FOREIGN KEY (marchandise_num) REFERENCES marchandises (num_marchandise),
+     FOREIGN KEY (client_num) REFERENCES clients (num_client)
+     );
