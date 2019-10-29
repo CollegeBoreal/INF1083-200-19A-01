f34ce138941ea4370c204dc0e1986bb365f6a098
widby <43479739+widby@users.noreply.github.com>
2019-10-29 14:11:58 -0400

    Update 300113775-data.sql

---
+++ 4.DML/300113775/300113775-data.sql
@@ -0,0 +1,8 @@
+-- Dans le domaine
+
+use picasa;
+
+-- Données de la table picasa
+
+INSERT INTO PICASA ( modes, format, reseaux_sociaux) VALUES (portrait , RGB, Facebook);
+INSERT INTO PICASA ( modes, format, reseaux_sociaux) VALUES (paysage, RGB, Instagram);
