581425f02904cd9a8d793b0c4cf2f9dfcc690d07
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-10-29 13:28:33 -0400

    .. :car: ..

--- 4.DML/300107361/300107361-data.sql
+++ 4.DML/300107361/300107361-data.sql
@@ -5,7 +5,7 @@ use Car_Center;
 -- Données des tables
 
 INSERT INTO SERVICES_OFFERED (Sale) VALUES ( 'Car_Trucks');
-INSERT INTO MARKS ( Mark ) VALUES ('Acura' , 'Audi', 'BMW' ,'Ferrari' , 'Ford' , 'GMC' , 'Honda' , 'Hyundai' , 'Infiniti' ,'Jaguar', 'Jeep','Lamborghini', 'Lexus', 'Nissan' , 'Porsche' , 'Toyota');
+INSERT INTO MARKS (Mark) VALUES ('Acura' , 'Audi', 'BMW' ,'Ferrari' , 'Ford' , 'GMC' , 'Honda' , 'Hyundai' , 'Infiniti' ,'Jaguar', 'Jeep','Lamborghini', 'Lexus', 'Nissan' , 'Porsche' , 'Toyota');
 INSERT INTO MODELS (Model) VALUES ('CL', 'LEGEND', 'CABRIOLET', 'GT_SPYDER', 'AVALANCHE', 'AVEO', 'IMPALA', 'CHEROKEE', 'WRANGLER');
 INSERT INTO ORIGINS (Origins) VALUES ('USA', 'Japan', 'Germany', 'Italy',);
 INSERT INTO COLOURS (Colours) VALUES ('Black', 'Bleue', 'Gray', 'white', 'green', 'red', 'yellow', '...');
