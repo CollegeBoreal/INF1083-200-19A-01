a3aef029b2b30291c9de01e9a4e00a438588e872
Hali-Ma <43480203+halimabzn@users.noreply.github.com>
2019-10-29 14:13:57 -0400

    👀

--- 4.DML/300108234/300108234-schema.sql
+++ 4.DML/300108234/300108234-schema.sql
@@ -16,7 +16,7 @@ use Hijabfashion;
 -- Mes tables
 
 
-CREATE TABLE Couleurs (
+CREATE TABLE COULEURS (
 couleur INT AUTO_INCREMENT,  
 nom VARCHAR(30) NOT NULL,
 PRIMARY KEY (couleur)
@@ -24,7 +24,7 @@ PRIMARY KEY (couleur)
 );
 
 
-CREATE TABLE Prix (
+CREATE TABLE PRIX (
 prix INT AUTO_INCREMENT,
 nom VARCHAR(30) NOT NULL,
 tarif VARCHAR(30) NOT NULL,
@@ -33,7 +33,7 @@ PRIMARY KEY (prix)
 );
 
 
-CREATE TABLE Tailles (   
+CREATE TABLE TAILLES (   
 taille INT AUTO_INCREMENT,
 largeur VARCHAR(30) NOT NULL,
 longeur VARCHAR(30) NOT NULL,
@@ -43,14 +43,14 @@ PRIMARY KEY (taille)
 );
 
 
-CREATE TABLE Tissus (
+CREATE TABLE TISSUS (
 tissu INT AUTO_INCREMENT,
 nom VARCHAR(30) NOT NULL, 
 PRIMARY KEY (tissu)
 );
 
 
-CREATE TABLE Accessoires (
+CREATE TABLE ACCESSOIRES (
 accessoire INT AUTO_INCREMENT,  
 nom VARCHAR(30) NOT NULL,
 type VARCHAR(30) NOT NULL,   
@@ -59,14 +59,14 @@ PRIMARY KEY (accessoire)
 );
 
 
-CREATE TABLE Pays_de_fabrication ( 
+CREATE TABLE PAYS_DE_FABRICATION ( 
 tissu INT,
 accessoire INT,
 pays_de_fabrication INT,
 PRIMARY KEY ( tissu, accessoire),
 FOREIGN KEY(tissu) 
-     REFERENCES Tissu(tissu),
+     REFERENCES TISSUS(tissu),
 FOREIGN KEY(accessoire) 
-     REFERENCES Accessoires(accessoire)
+     REFERENCES ACCESSOIRES(accessoire)
  
  );
