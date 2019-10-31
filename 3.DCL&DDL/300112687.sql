95b080cfc2d25c07fd358b78b6b190429e444584
jthn9022 <43135622+jthn9022@users.noreply.github.com>
2019-10-31 13:01:01 -0400

    Update 300112687.sql

--- 3.DCL&DDL/300112687.sql
+++ 3.DCL&DDL/300112687.sql
@@ -1,50 +1,55 @@
 -- MON de Domaine
 
+
 CREATE DATABASE Assurance;
 
 -- Mon  Utilisateur
 
+
  CREATE USER 'Assurance'@'localhost' IDENTIFIED BY 'Assurance_1';
- 
- 
  GRANT ALL ON Assurance.* TO 'Assurrance'@'localhost';
  
  -- Selectionne la base de donnees
  
  
- use Assurance;
- 
+ use Assurance 
  
  -- Mes Tables
 
   
- CREATE TABLE Assurance (
- NAME VARCHAR(33) NOT NULL
-  
+ CREATE TABLE Clients (
+  num_clinet INT NOT NULL (23) 
+  nom VARCHAR(33) NOT NULL
+ 
  );
+ 
  
  CREATE TABLE ABONEMENT (
- NAME VARCHAR (30) NOT NULL
- MONTHS int
   
+ nom VARCHAR (30) NOT NULL,
+       mois INT(24) NOT NULL, 
+ type d'abonement VARCHAR (24) NOT NULL 
  );
  
  
- CREATE TABLE PAYEMENT(
- NAME VARCHAR (30) NOT NULL
- type OF PAYEMENT VARCHAR (30) NOT NULL
+ CREATE TABLE PAIEMENT  (
+    nom VARCHAR(40) NOT NULL,
   
+   num_client INT NOT NULL ,
+  mois VARCHAR (40) NOT NULL 
  );
- 
  
  CREATE TABLE SERVICES(
- Initale VARCHAR (59) NOT NULL
- Type OF SERVICES VARCHAR (30) NOT NULL
- 
+  
+    name VARCHAR (25) NOT NULL,
+ Type de services VARCHAR (30) NOT NULL
  );
  
- CREATE TABLE PRICES (
- Initiale VARCHAR (44)
-  PRICES int
+ CREATE TABLE Prices (
+  
+ nom VARCHAR (25) NOT NULL,
+ prix INT (14)
   
  );
+  
+ 
