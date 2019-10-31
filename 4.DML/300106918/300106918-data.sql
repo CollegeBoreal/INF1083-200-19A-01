1b3c02286b6abe3746b7be35c75be6a48e84045b
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2019-10-31 04:19:30 -0400

    Update 300106918-data.sql

--- 4.DML/300106918/300106918-data.sql
+++ 4.DML/300106918/300106918-data.sql
@@ -1,1 +1,28 @@
 
+-- Dans le domaine
+
+use network_IT;
+
+-- Données de la table SOLUTIONS
+
+INSERT INTO SOLUTIONS ( solution, nom,  ) VALUES ( 'maintenance , 55);
+INSERT INTO SOLUTIONS ( solution, nom,  ) VALUES ( 'configuration', 15);
+INSERT INTO SOLUTIONS ( solution, nom,  ) VALUES ('vlan' , 75);
+INSERT INTO SOLUTIONS ( solution, nom,  ) VALUES ('routeur', 100);
+
+-- Données de la table product
+
+INSERT INTO PRODUCTS ( nom, Date ) VALUES ('routers');
+INSERT INTO PRODUCTS ( nom, Date ) VALUES ('switches');
+INSERT INTO PRODUCTS ( nom, Date ) VALUES ('camera' );
+INSERT INTO PRODUCTS ( nom, Date ) VALUES ('cables' );
+
+
+-- Données de la table insttalation
+
+INSERT INTO INSTTALATIONS (nom ) VALUES ('firewall');
+INSERT INTO INSTTALATIONS (nom ) VALUES ('anti-virus');
+INSERT INTO INSTTALATIONS (nom ) VALUES ('mise a jours');
+                                                  
+  -- Données de la table prix
+INSERT INTO PRIX (solution, installation,prix ) VALUES ('100, 2000, 1500,');
