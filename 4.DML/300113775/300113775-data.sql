2f4c60d30b479e7894d500b26d3c4b3702b3a064
widby <43479739+widby@users.noreply.github.com>
2019-10-31 12:23:49 -0400

    Update 300113775-data.sql

--- 4.DML/300113775/300113775-data.sql
+++ 4.DML/300113775/300113775-data.sql
@@ -6,5 +6,5 @@ use widesign;
 
 -- Données de la table picasa
 
-INSERT INTO IMAGES (image ) VALUES ( '1');
-INSERT INTO USES (utilisation ) VALUES ('5');
+INSERT INTO IMAGES (name) VALUES ( 'portrait');
+INSERT INTO USES (social_networks) VALUES ('facebook');
