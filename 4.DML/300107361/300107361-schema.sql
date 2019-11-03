41cc66528167a900246714ccd0af6e187bbd20ae
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-11-03 17:59:30 -0500

    _:car:__:car:_

--- 4.DML/300107361/300107361-schema.sql
+++ 4.DML/300107361/300107361-schema.sql
@@ -11,30 +11,38 @@ use Car_Center;
 
 
 CREATE TABLE SERVICES_OFFERED (
-  services INT AUTO_INCREMENT,
-  Sale VARCHAR(30) NOT NULL,
-  PRIMARY KEY(services)
+  service_offered INT AUTO_INCREMENT,
+  Sale TEXT NOT NULL,
+  PRIMARY KEY(service_offered)
   );
   
 CREATE TABLE MARKS (
   mark INT AUTO_INCREMENT,
-  Marks VARCHAR(500) NOT NULL,
-  PRIMARY KEY(mark)
+  origin INT,
+  Marks VARCHAR(30) NOT NULL,
+  PRIMARY KEY(mark, origin),
+    FOREIGN KEY(origin)
+      REFERENCES ORIGINS(origin)
   );
   
 CREATE TABLE MODELS (
    model INT AUTO_INCREMENT,
    mark INT,
-   Models VARCHAR(500) NOT NULL,
+   colour INT,
+   year INT,
+   Models VARCHAR(50) NOT NULL,
    PRIMARY KEY(model, mark),
      FOREIGN KEY(mark)
-       REFERENCES MARKS(mark)
+       REFERENCES MARKS(mark),
+     FOREIGN KEY(colour)
+       REFERENCES COLOURS(colour),
+     FOREIGN KEY(year)
+       REFERENCES YEARS(year)
    );
 
 CREATE TABLE ORIGINS (
    origin INT AUTO_INCREMENT,
-   mark INT,
-   Origins VARCHAR(255) NOT NULL,
+   Origins TEXT NOT NULL,
    PRIMARY KEY(origin),
      FOREIGN KEY(mark)
        REFERENCES MARKS(mark)
@@ -44,14 +52,17 @@ CREATE TABLE ORIGINS (
 CREATE TABLE COLOURS (
    colour INT AUTO_INCREMENT,
    Colours VARCHAR(255) NOT NULL,
-   PRIMARY KEY(colour)
+   model INT,
+   PRIMARY KEY(colour),
+     FOREIGN KEY(model)
+       REFERENCES MODELS(model)
    );
    
 CREATE TABLE YEARS (
   year INT  AUTO_INCREMENT,
   model INT,
   Years DATE,
-  PRIMARY KEY(year, model),
+  PRIMARY KEY(year),
      FOREIGN KEY(model)
         REFERENCES MODELS(model)
   );
@@ -66,16 +77,22 @@ CREATE TABLE PRICES (
   price INT,
   model INT,
   mark INT,
+  year INT,
   PRIMARY KEY(model, mark),
   FOREIGN KEY(model)
      REFERENCES MODELS(model),
   FOREIGN KEY(mark)
-     REFERENCES MARKS(mark)
+     REFERENCES MARKS(mark),
+  FOREIGN KEY(year)
+     REFERENCES YEARS(year)
   );
 
 CREATE TABLE MAINTENANCES (
   maintenance INT AUTO_INCREMENT,
+  service_offered INT,
   Maintenances VARCHAR(255) NOT NULL,
-  PRIMARY KEY(maintenance)
+  PRIMARY KEY(maintenance),
+    FOREIGN KEY(service_offered)
+       REFERENCES SERVICES_OFFERED(service_offered)
   );
   
