201f2cbe271c27148e51c3fbf2d406ddf04b34fd
didier300110500 <43076393+didier300110500@users.noreply.github.com>
2019-11-05 14:44:02 -0500

    Update 300110500-data.sql

--- 4.DML/300110500/300110500-data.sql
+++ 4.DML/300110500/300110500-data.sql
@@ -8,6 +8,6 @@ INSERT INTO TYPE_VISAS (Type_visa, Name, Etudiant) VALUES ('VE', 'Hamidou_Zongo'
 INSERT INTO TYPE_VISAS (Type_visa, Name, Resident_permanent) VALUES ('VRP', 'Cristiano_Ronaldo', 'POR');
 INSERT INTO HONORAIRE_PAYMENTS ( Name, Periode, Especes) VALUES ('Reglement', '18-10-2019', '1000$');
 
-INSERT INTO VILLE_RESIDENT (Name Zone_afrique) VALUES ('Hamidou_zongo', 'Congo'); 
+INSERT INTO VILLE_RESIDENCE (Name Zone_afrique) VALUES ('Hamidou_zongo', 'Congo'); 
 INSERT INTO TYPE_SERVICES (Type_visa, Honoraire_payment, Type _services) VALUES ('10', '1000', '1');
 
