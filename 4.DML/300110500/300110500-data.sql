e80b3896017f8f7b3ba7bdd7fcc24ef9fa8120d8
didier300110500 <43076393+didier300110500@users.noreply.github.com>
2019-11-05 16:06:56 -0500

    Update 300110500-data.sql

--- 4.DML/300110500/300110500-data.sql
+++ 4.DML/300110500/300110500-data.sql
@@ -4,10 +4,15 @@ use immigration_consulting;
 
 -- Données des tables
 
-INSERT INTO TYPE_VISAS (Type_visa, Name, Etudiant) VALUES ('VE', 'Hamidou_Zongo', 'BFA');
-INSERT INTO TYPE_VISAS (Type_visa, Name, Resident_permanent) VALUES ('VRP', 'Cristiano_Ronaldo', 'POR');
-INSERT INTO HONORAIRE_PAYMENTS ( Name, Periode, Especes) VALUES ('Reglement_demande_officiellle', '18-10-2019', '1000$');
+INSERT INTO TYPE_VISAS (Type_visa, Name, Etudiant) VALUES ('BFA', 'Hamidou_Zongo', 'VE');
+INSERT INTO TYPE_VISAS (Type_visa, Name, Resident_permanent) VALUES ('POR', 'Cristiano_Ronaldo', 'VRP');
 
-INSERT INTO VILLE_RESIDENCE (Name Zone_afrique) VALUES ('Hamidou_zongo', 'Congo'); 
-INSERT INTO TYPE_SERVICES (Type_visa, Honoraire_payment, Type _services) VALUES ('10', '1000', '1');
+INSERT INTO HONORAIRE_PAYMENTS ( Name, Periode, Especes) VALUES ('Reglement_partiel', '18-10-2019', '1000$');
+INSERT INTO HONORAIRE_PAYMENTS ( Name, Periode, Especes) VALUES ('Reglement_pour_solde', '31-10-2019', '1000$');
+
+INSERT INTO VILLE_RESIDENCE (Name Zone_afrique) VALUES ('Hamidou_zongo', 'Congo_Afrique'); 
+INSERT INTO VILLE_RESIDENCE (Name Zone_europe) VALUES ('Cristiano_Ronaldo', 'Portugal_europe'); 
+
+INSERT INTO TYPE_SERVICES (Type_service, intituless) VALUES ('Etudiant_international', 'Temporaire');
+INSERT INTO TYPE_SERVICES (Type_service, Duree) VALUES ('Travailleur_qualifie', 'permanent');
 
