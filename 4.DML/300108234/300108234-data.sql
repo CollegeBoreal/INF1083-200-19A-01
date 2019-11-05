15bfde8509e24dfc5d287bc7e2aa151069f3a4dc
Hali-Ma <43480203+halimabzn@users.noreply.github.com>
2019-11-05 15:24:06 -0500

    🙌

--- 4.DML/300108234/300108234-data.sql
+++ 4.DML/300108234/300108234-data.sql
@@ -4,25 +4,25 @@ use Hijabfashion;
 
 -- Données de la table couleurs
 
-INSERT INTO Couleurs ( nom ) VALUES ( 'Noir');
+INSERT INTO COULEURS ( nom ) VALUES ( 'Noir');
 
 -- Données de la table prix
 
-INSERT INTO Prix ( nom, tarif ) VALUES ( 10, 13%);
+INSERT INTO PRIX ( nom, tarif ) VALUES ( 10, 13%);
 
 -- Données de la table tailles
 
-INSERT INTO Tailles ( Largeur, Longeur, Unite_de_mesure ) VALUES ( 55, 160, 'cm');
+INSERT INTO TAILLES ( Largeur, Longeur, Unite_de_mesure ) VALUES ( 55, 160, 'cm');
 
 -- Données de la table tissu
 
-INSERT INTO Tissu ( nom) VALUES ( 'Cotton');
+INSERT INTO TISSUS ( nom) VALUES ( 'Cotton');
 
 -- Données de la table accessoires
 
-INSERT INTO Accessoires ( nom, type ) VALUES ( 'Perle','Coloree');
+INSERT INTO ACCESSOIRES ( nom, type ) VALUES ( 'Perle','Coloree');
 
 -- Données de la table pays de fabrication
 
-INSERT INTO Pays_de_fabrication ( nom, devise) VALUES ( 'Maroc', 'Dirham');
+INSERT INTO PAYS_DE_FABRICATION ( nom, devise) VALUES ( 'Maroc', 'Dirham');
 
