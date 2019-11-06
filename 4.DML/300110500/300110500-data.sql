4764f36f543c7fb262a892b912645ca789eda5c1
didier300110500 <43076393+didier300110500@users.noreply.github.com>
2019-11-06 14:02:48 -0500

    Update 300110500-data.sql

--- 4.DML/300110500/300110500-data.sql
+++ 4.DML/300110500/300110500-data.sql
@@ -10,8 +10,8 @@ INSERT INTO TYPE_VISAS (Type_visa, Name, Resident_permanent) VALUES ('POR', 'Cri
 INSERT INTO HONORAIRE_PAYMENTS ( Name, Periode, Especes) VALUES ('Reglement_partiel', '18-10-2019', '1000$');
 INSERT INTO HONORAIRE_PAYMENTS ( Name, Periode, Especes) VALUES ('Reglement_pour_solde', '31-10-2019', '1000$');
 
-INSERT INTO VILLE_RESIDENCE (Name Zone_afrique) VALUES ('Hamidou_zongo', 'Congo_Afrique'); 
-INSERT INTO VILLE_RESIDENCE (Name Zone_europe) VALUES ('Cristiano_Ronaldo', 'Portugal_europe'); 
+INSERT INTO VILLE_RESIDENCE (Name, Pays, Continent) VALUES ('Hamidou_zongo', 'Congo', 'Afrique'); 
+INSERT INTO VILLE_RESIDENCE (Name,Pays, Continent ) VALUES ('Cristiano_Ronaldo', 'Portugal', 'Europe'); 
 
 INSERT INTO TYPE_SERVICES (Type_service, Duree) VALUES ('Etudiant_international', 'Temporaire');
 INSERT INTO TYPE_SERVICES (Type_service, Duree) VALUES ('Travailleur_qualifie', 'permanent');
