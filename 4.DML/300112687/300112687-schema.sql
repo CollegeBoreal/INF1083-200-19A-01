0b6c4d76d3477bb3e7bd244da16281e1e127cab1
jthn9022 <43135622+jthn9022@users.noreply.github.com>
2019-11-06 17:49:46 -0500

    Update 300112687-schema.sql

--- 4.DML/300112687/300112687-schema.sql
+++ 4.DML/300112687/300112687-schema.sql
@@ -33,13 +33,15 @@ CREATE DATABASE Assurance;
  
  
  CREATE TABLE PAIEMENT  (
+  
     nom VARCHAR(40) NOT NULL,
    num_client INT(25) NOT NULL PRIMARY KEY,
   mois VARCHAR (40) NOT NULL 
  );
  
- CREATE TABLE SERVICES(
-    nom VARCHAR (25) NOT NULL,
+ CREATE TABLE SERVICES  (
+  
+   nom VARCHAR (25) NOT NULL,
  Type de services VARCHAR (30) NOT NULL
  );
  
