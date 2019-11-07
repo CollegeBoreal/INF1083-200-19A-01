148040880f198b301256b5b555c8f544da8823d4
AbbasSadissou <43480195+AbbasSadissou@users.noreply.github.com>
2019-11-07 12:25:16 -0500

    Update 300111671-data.sql

---
+++ 4.DML/300111671/300111671-data.sql
@@ -0,0 +1,85 @@
+-- Dans le domaine
+
+
+
+use Boubou;
+
+
+
+-- Données de la table pays
+
+
+
+INSERT INTO PAYS ( nom ) VALUES ( 'senegal');
+INSERT INTO PAYS ( nom ) VALUES ( 'benin');
+INSERT INTO PAYS ( nom ) VALUES ( 'mali');
+
+
+-- Données de la table couleur
+
+
+
+INSERT INTO COULEUR ( nom ) VALUES ( 'noir');
+INSERT INTO COULEUR ( nom ) VALUES ( 'bleu');
+INSERT INTO COULEUR ( nom ) VALUES ( 'rouge');
+
+
+
+
+-- Données de la table genre
+
+
+
+INSERT INTO genre ( nom, type ) VALUES ( 'dashiki');
+INSERT INTO genre ( nom, type ) VALUES ( 'alabe');
+
+-- Données de la table pays de modele
+
+
+
+INSERT INTO MODELE ( nom ) VALUES ( 'senegalais');
+INSERT INTO MODELE ( nom ) VALUES ( 'benioise');
+INSERT INTO MODELE ( nom ) VALUES ( 'malienne');
+
+
+-- Données de la table paiement
+
+
+INSERT INTO PAIEMENT ( nom ) VALUES ( 'debit card');
+INSERT INTO PAIEMENT ( nom ) VALUES ( 'mastercard',);
+INSERT INTO PAIEMENT ( nom ) VALUES ( 'paypal');
+
+
+-- Données de la table duree
+
+
+INSERT INTO DUREE ( nom ) VALUES ( '24 heures');
+INSERT INTO DUREE ( nom ) VALUES ( '3 jours ');
+INSERT INTO DUREE ( nom ) VALUES ( '1 semaine');
+
+
+
+-- Données de la table quantite
+
+
+INSERT INTO QUANTITE ( nom ) VALUES ( '1 a 3');
+INSERT INTO QUANTITE ( nom ) VALUES ( '1 douzaine ');
+INSERT INTO QUANTITE ( nom ) VALUES ( '2 douzaine ');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
