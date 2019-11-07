e670d8fb77966b2d3c10fe737bd840417dcc95e3
BGBG <43278130+BgbgL13@users.noreply.github.com>
2019-11-07 05:17:54 -0800

    Update 300105201-data.sql

---
+++ 4.DML/300105201/300105201-data.sql
@@ -0,0 +1,15 @@
+-- Dans le domaine
+
+use Musicshop;
+
+-- Données des tables
+
+INSERT INTO SERVICES_OFFERED (Sale) VALUES ('Keyboard');
+INSERT INTO SERVICES_OFFERED (Sale) VALUES ('Piano');
+INSERT INTO SERVICES_OFFERED (Sale) VALUES ('Module');
+
+INSERT INTO COLOURS (Colours, model) VALUES ('Blue', 5);
+INSERT INTO COLOURS (Colours, model) VALUES ('Red', 4);
+INSERT INTO COLOURS (Colours, model) VALUES ('White', 4);
+
+
