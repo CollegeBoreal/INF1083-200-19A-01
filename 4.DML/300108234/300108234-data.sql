6b45cdf2ca06cd732e6a415350b48afbda7c0e85
Hali-Ma <43480203+halimabzn@users.noreply.github.com>
2019-11-07 11:42:31 -0500

    👀

--- 4.DML/300108234/300108234-data.sql
+++ 4.DML/300108234/300108234-data.sql
@@ -33,7 +33,7 @@ INSERT INTO PAYS_DE_FABRICATION ( nom, devise, livraison ) VALUES ( 'Arabie saou
 
 -- Données de la table tissu
 
-INSERT INTO TISSUS ( taille, prix, couleur, accessoire, pays_de_fabrication, tissu) VALUES ( 1, 2, 1, 1, 3,'Cotton');
+INSERT INTO TISSUS ( taille, prix, couleur, accessoire, pays_de_fabrication, tissu) VALUES ( 1, 2, 1, 1, 3,'cotton');
 INSERT INTO TISSUS ( taille, prix, couleur, accessoire, pays_de_fabrication, tissu) VALUES ( 3, 3, 3, 1, 2,'laine');
 INSERT INTO TISSUS ( taille, prix, couleur, accessoire, pays_de_fabrication, tissu) VALUES ( 2, 1, 2, 2, 1,'soie');
 
