d1c21e1463f92ec65f03f2ed336e380675f715d1
Hali-Ma <43480203+halimabzn@users.noreply.github.com>
2019-11-07 11:26:06 -0500

    👀

--- 4.DML/300108234/300108234-data.sql
+++ 4.DML/300108234/300108234-data.sql
@@ -5,26 +5,45 @@ use Hijabfashion;
 -- Données de la table couleurs
 
 INSERT INTO COULEURS ( nom ) VALUES ( 'noir');
+INSERT INTO COULEURS ( nom ) VALUES ( 'pale');
+INSERT INTO COULEURS ( nom ) VALUES ( 'bleu');
 
 -- Données de la table prix
 
 INSERT INTO PRIX ( nom, tarif ) VALUES ( 10, 5);
+INSERT INTO PRIX ( nom, tarif ) VALUES ( 15, 5);
+INSERT INTO PRIX ( nom, tarif ) VALUES ( 20, 5);
 
 -- Données de la table tailles
 
-INSERT INTO TAILLES ( largeur, longeur, unite_de_mesure ) VALUES ( 55, 160, 'cm');
+INSERT INTO TAILLES ( largeur, longeur, unite_de_mesure ) VALUES ( 55, 140, 'cm');
+INSERT INTO TAILLES ( largeur, longeur, unite_de_mesure ) VALUES ( 60, 160, 'cm');
+INSERT INTO TAILLES ( largeur, longeur, unite_de_mesure ) VALUES ( 65, 180, 'cm');
 
 -- Données de la table accessoires
 
 INSERT INTO ACCESSOIRES ( nom, type ) VALUES ( 'perle','coloree');
+INSERT INTO ACCESSOIRES ( nom, type ) VALUES ( 'Motif','broderie');
 
 -- Données de la table pays de fabrication
 
-INSERT INTO PAYS_DE_FABRICATION ( nom, devise ) VALUES ( 'maroc', 'dirham');
+INSERT INTO PAYS_DE_FABRICATION ( nom, devise, livraison ) VALUES ( 'maroc', 'dirham', '2019-11-15');
+INSERT INTO PAYS_DE_FABRICATION ( nom, devise, livraison ) VALUES ( 'yeman', 'riyal', '2019-11-30');
+INSERT INTO PAYS_DE_FABRICATION ( nom, devise, livraison ) VALUES ( 'Arabie saoudite', 'riyal', '2019-12-30');
 
 -- Données de la table tissu
 
 INSERT INTO TISSUS ( taille, prix, couleur, accessoire, pays_de_fabrication,
-tissu ) VALUES ( 55,160, 'cm', 10,5, 'noir', 'perle', 'coloree', 'maroc', 'Cotton');
+tissu) VALUES ( 1, 2, 1, 1, 3,'Cotton');
+INSERT INTO TISSUS ( taille, prix, couleur, accessoire, pays_de_fabrication,
+tissu) VALUES ( 3, 3, 3, 1, 2,'laine');
+INSERT INTO TISSUS ( taille, prix, couleur, accessoire, pays_de_fabrication,
+tissu) VALUES ( 2, 1, 2, 2, 1,'soie');
 
+INSERT INTO ADRESSES ( rue, ville, pays, code_postal ) VALUES ( '1765 victoria park drive', 'toronto', 'canada', 'm1s3r4');
+INTO ADRESSES ( rue, ville, pays, code_postal ) VALUES ( '89 amal', 'algerie', 'algerie', '2456');
+INTO ADRESSES ( rue, ville, pays, code_postal ) VALUES ( '1254 river street', 'chicago', 'usa', 's4r1m2);
 
+INSERT INTO CLIENTS ( adresse, client ) VALUES ( 1, 'aya samar');
+INSERT INTO CLIENTS ( adresse, client ) VALUES ( 3, 'nada ashkar');
+INSERT INTO CLIENTS ( adresse, client ) VALUES ( 2, 'maria amr');
