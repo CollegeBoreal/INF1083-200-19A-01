43a1c26beb3c4596251f937a0fdbbac3897a967e
widby <43479739+widby@users.noreply.github.com>
2019-11-07 23:38:52 -0500

    Update 300113775-data.sql

--- 4.DML/300113775/300113775-data.sql
+++ 4.DML/300113775/300113775-data.sql
@@ -1,44 +1,54 @@
+  
 
 
 -- Dans le domaine
 
+
+
 use widesign;
 
--- Données de widesign
 
 
--- Données de la tables IMAGES
-INSERT INTO IMAGES (name, image) VALUES ( 'portrait' 1);
-INSERT INTO IMAGES (name, image) VALUES ( 'paysage' 2);
-INSERT INTO IMAGES (name, image) VALUES ( 'flyer'3);
+-- Données de la base widesign
 
 
--- Données de la tables USES
-INSERT INTO USES (social_networks,utilisation) VALUES ('facebook', 1);
-INSERT INTO USES (social_networks,utilisation) VALUES ('instagram', 2);
-INSERT INTO USES (social_networks,utilisation ) VALUES ('linkedin', 3);
+INSERT INTO IMAGES (name) VALUES ( 'portrait');
+
+INSERT INTO IMAGES (name) VALUES ( 'paysage');
+
+INSERT INTO IMAGES (name) VALUES ( 'flyer');
 
 
--- Données de la tables DELIVERIES
-INSERT INTO DELIVERIES  (shipping) VALUES ('2019-03-25');
-INSERT INTO  DELIVERIES (shipping) VALUES ('2019-08-12');
-INSERT INTO DELIVERIES  (shipping) VALUES ('2019-03-25');
-INSERT INTO  DELIVERIES (shipping) VALUES ('2019-09-17');
-INSERT INTO DELIVERIES  (shipping) VALUES ('2019-03-24');
-INSERT INTO  DELIVERIES (shipping) VALUES ('2019-04-21');
-INSERT INTO DELIVERIES  (shipping) VALUES ('2019-07-09');
-INSERT INTO  DELIVERIES (shipping) VALUES ('2019-05-14');
+INSERT INTO USES (social_networks) VALUES ('facebook');
 
--- Données de la tables COLORS
-INSERT INTO COLORS (color) VALUES ('blue');
-INSERT INTO COLORS (color) VALUES ('black n white');
-INSERT INTO COLORS (color) VALUES ('sepian');
-INSERT INTO COLORS (color) VALUES ('original');
+INSERT INTO USES (social_networks) VALUES ('instagram');
+
+INSERT INTO USES (social_networks) VALUES ('linkedin');
 
 
--- Données de la tables PRICES 
-INSERT INTO PRICES (price) VALUES (200);
-INSERT INTO PRICES (price) VALUES (100);
-INSERT INTO PRICES (price) VALUES (250);
-INSERT INTO PRICES (price) VALUES (180);
-INSERT INTO PRICES (price) VALUES (150);
+
+INSERT INTO COLORS(color) VALUES ('blue');
+
+INSERT INTO COLORS(color) VALUES ('original');
+
+INSERT INTO COLORS(color) VALUES ('blacknwhite');
+
+INSERT INTO COLORS(color) VALUES ('sepian');
+
+INSERT INTO COLORS(color) VALUES ('mono');
+
+INSERT INTO COLORS(color) VALUES ('dramatic');
+
+INSERT INTO COLORS(color) VALUES ('argentic');
+
+
+INSERT INTO SHIPPINGS(shipping) VALUES (1);
+INSERT INTO SHIPPINGS(shipping) VALUES (2);
+INSERT INTO SHIPPINGS(shipping) VALUES (3);
+INSERT INTO SHIPPINGS(shipping) VALUES (4);
+INSERT INTO SHIPPINGS(shipping) VALUES (5);
+
+INSERT INTO SHIPPINGS(shipp) VALUES ('2019-03-25');
+
+
+
