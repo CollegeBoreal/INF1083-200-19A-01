0efeac2a15a8dd689b0a6f3318389fd2c9119cf9
widby <43479739+widby@users.noreply.github.com>
2019-11-07 21:12:35 -0500

    Update 300113775-data.sql

--- 4.DML/300113775/300113775-data.sql
+++ 4.DML/300113775/300113775-data.sql
@@ -16,29 +16,29 @@ INSERT INTO IMAGES (name) VALUES ( 'flyer');
 -- Données de la tables USES
 INSERT INTO USES (social_networks,utilisation) VALUES ('facebook', 1);
 INSERT INTO USES (social_networks,utilisation) VALUES ('instagram', 2);
-INSERT INTO USES (social_networks,utilisation ) VALUES ('linkedin', 3)
+INSERT INTO USES (social_networks,utilisation ) VALUES ('linkedin', 3);
 
 
 -- Données de la tables DELIVERIES
-INSERT INTO DELIVERIES  (shipping) VALUES ('2019-03-25')
-INSERT INTO  DELIVERIES (shipping) VALUES ('2019-08-12')
-INSERT INTO DELIVERIES  (shipping) VALUES ('2019-03-25')
-INSERT INTO  DELIVERIES (shipping) VALUES ('2019-09-17')
-INSERT INTO DELIVERIES  (shipping) VALUES ('2019-03-24')
-INSERT INTO  DELIVERIES (shipping) VALUES ('2019-04-21')
-INSERT INTO DELIVERIES  (shipping) VALUES ('2019-07-09')
-INSERT INTO  DELIVERIES (shipping) VALUES ('2019-05-14')
+INSERT INTO DELIVERIES  (shipping) VALUES ('2019-03-25');
+INSERT INTO  DELIVERIES (shipping) VALUES ('2019-08-12');
+INSERT INTO DELIVERIES  (shipping) VALUES ('2019-03-25');
+INSERT INTO  DELIVERIES (shipping) VALUES ('2019-09-17');
+INSERT INTO DELIVERIES  (shipping) VALUES ('2019-03-24');
+INSERT INTO  DELIVERIES (shipping) VALUES ('2019-04-21');
+INSERT INTO DELIVERIES  (shipping) VALUES ('2019-07-09');
+INSERT INTO  DELIVERIES (shipping) VALUES ('2019-05-14');
 
 -- Données de la tables COLORS
-INSERT INTO COLORS (color) VALUES ('blue')
-INSERT INTO COLORS (color) VALUES ('black n white')
-INSERT INTO COLORS (color) VALUES ('sepian')
-INSERT INTO COLORS (color) VALUES ('original')
+INSERT INTO COLORS (color) VALUES ('blue');
+INSERT INTO COLORS (color) VALUES ('black n white');
+INSERT INTO COLORS (color) VALUES ('sepian');
+INSERT INTO COLORS (color) VALUES ('original');
 
 
 -- Données de la tables PRICES 
-INSERT INTO PRICES (price) VALUES (200)
-INSERT INTO PRICES (price) VALUES (100)
-INSERT INTO PRICES (price) VALUES (250)
-INSERT INTO PRICES (price) VALUES (180)
-INSERT INTO PRICES (price) VALUES (150)
+INSERT INTO PRICES (price) VALUES (200);
+INSERT INTO PRICES (price) VALUES (100);
+INSERT INTO PRICES (price) VALUES (250);
+INSERT INTO PRICES (price) VALUES (180);
+INSERT INTO PRICES (price) VALUES (150);
