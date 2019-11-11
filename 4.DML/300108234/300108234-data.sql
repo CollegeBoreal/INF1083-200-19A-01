3a81e04a5b7aec5de4e5119293eb48890b26e935
Hali-Ma <43480203+halimabzn@users.noreply.github.com>
2019-11-10 19:55:59 -0500

    🤷‍♀️

--- 4.DML/300108234/300108234-data.sql
+++ 4.DML/300108234/300108234-data.sql
@@ -2,17 +2,39 @@
 
 use Hijabfashion;
 
--- Données de la table couleurs
 
-INSERT INTO COULEURS ( nom ) VALUES ( 'noir');
-INSERT INTO COULEURS ( nom ) VALUES ( 'pale');
-INSERT INTO COULEURS ( nom ) VALUES ( 'bleu');
+-- Données de la table ventes
+
+INSERT INTO VENTES ( vente, commande ) VALUES ( 1,  '2019-11-10');
+INSERT INTO VENTES ( vente, commande ) VALUES ( 2,  '2019-11-15');
+INSERT INTO VENTES ( vente, commande ) VALUES ( 3,  '2019-11-30');
 
 -- Données de la table prix
 
 INSERT INTO PRIX ( nom, tarif ) VALUES ( 10, 5);
 INSERT INTO PRIX ( nom, tarif ) VALUES ( 15, 5);
 INSERT INTO PRIX ( nom, tarif ) VALUES ( 20, 5);
+
+
+-- Données de la table adresses
+
+INSERT INTO ADRESSES ( rue, ville, pays, code_postal ) VALUES ( '1765 victoria park drive', 'toronto', 'canada', 'm1s3r4');
+INSERT INTO ADRESSES ( rue, ville, pays, code_postal ) VALUES ( '89 amal', 'oran', 'algerie', 2456);
+INSERT INTO ADRESSES ( rue, ville, pays, code_postal ) VALUES ( '1254 river street', 'chicago', 'usa', 's4r1m2');
+
+-- Données de la table clients
+
+INSERT INTO CLIENTS ( vente, prix, adresse, client ) VALUES ( 1, 1, 1, 45);
+INSERT INTO CLIENTS ( adresse, client ) VALUES ( 3, 2, 2, 55);
+INSERT INTO CLIENTS ( adresse, client ) VALUES ( 2, 3, 3, 60);
+
+
+
+-- Données de la table couleurs
+
+INSERT INTO COULEURS ( nom ) VALUES ( 'noir');
+INSERT INTO COULEURS ( nom ) VALUES ( 'pale');
+INSERT INTO COULEURS ( nom ) VALUES ( 'bleu');
 
 -- Données de la table tailles
 
@@ -33,18 +55,7 @@ INSERT INTO PAYS_DE_FABRICATION ( nom, devise, livraison ) VALUES ( 'arabie saou
 
 -- Données de la table tissus
 
-INSERT INTO TISSUS ( taille, prix, couleur, accessoire, pays_de_fabrication, nom) VALUES ( 1, 2, 1, 1, 3, 'coton');
-INSERT INTO TISSUS ( taille, prix, couleur, accessoire, pays_de_fabrication, nom) VALUES ( 3, 3, 3, 1, 2, 'laine');
-INSERT INTO TISSUS ( taille, prix, couleur, accessoire, pays_de_fabrication, nom) VALUES ( 2, 1, 2, 2, 1, 'soie');
+INSERT INTO TISSUS ( taille, couleur, accessoire, pays_de_fabrication, nom) VALUES ( 1, 1, 1, 3, 1);
+INSERT INTO TISSUS ( taille, couleur, accessoire, pays_de_fabrication, nom) VALUES ( 3, 3, 1, 2, 13);
+INSERT INTO TISSUS ( taille, couleur, accessoire, pays_de_fabrication, nom) VALUES ( 2, 2, 2, 1, 18);
 
--- Données de la table adresses
-
-INSERT INTO ADRESSES ( rue, ville, pays, code_postal ) VALUES ( '1765 victoria park drive', 'toronto', 'canada', 'm1s3r4');
-INSERT INTO ADRESSES ( rue, ville, pays, code_postal ) VALUES ( '89 amal', 'oran', 'algerie', 2456);
-INSERT INTO ADRESSES ( rue, ville, pays, code_postal ) VALUES ( '1254 river street', 'chicago', 'usa', 's4r1m2');
-
--- Données de la table clients
-
-INSERT INTO CLIENTS ( adresse, client ) VALUES ( 1, 'aya samar');
-INSERT INTO CLIENTS ( adresse, client ) VALUES ( 3, 'nada ashkar');
-INSERT INTO CLIENTS ( adresse, client ) VALUES ( 2, 'maria amr');
