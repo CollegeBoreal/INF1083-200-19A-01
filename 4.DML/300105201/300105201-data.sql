f185d9f609b7c34b4d93636293aa106c3efc0871
BGBG <43278130+BgbgL13@users.noreply.github.com>
2019-11-12 11:11:31 -0800

    Update 300105201-data.sql

--- 4.DML/300105201/300105201-data.sql
+++ 4.DML/300105201/300105201-data.sql
@@ -9,11 +9,11 @@ INSERT INTO SERVICES_OFFERED (service_offered, Services) VALUES (1, 'Instruments
 INSERT INTO CUSTOMERS (customer, Name, Phone_number) VALUES (1, John, 6477765643);
 
 INSERT INTO INSTRUMENTS (instrument, Types, Name) VALUES (1, 'Percusion', 'Piano');
-INSERT INTO INSTRUMENTS (instrument, Types, Name) VALUES (2, 'Cordes', 'Guitar');
+INSERT INTO INSTRUMENTS (instrument, Types, Name) VALUES (2, 'Strings', 'Guitar');
 
-INSERT INTO COLOURS (colour, Colors) VALUES (1, 'White');
-INSERT INTO COLOURS (colour, Colors) VALUES (2, 'Black');
+INSERT INTO COLORS (color, Colors) VALUES (1, 'White');
+INSERT INTO COLORS (color, Colors) VALUES (2, 'Black');
 
-INSERT INTO  PRICES (price, instrument, colour) VALUES (7000, 1, 2);
-INSERT INTO  PRICES (price, instrument, colour) VALUES (5000, 2, 1);
+INSERT INTO  PRICES (price, instrument, color) VALUES (7000, 1, 2);
+INSERT INTO  PRICES (price, instrument, color) VALUES (5000, 2, 1);
 
