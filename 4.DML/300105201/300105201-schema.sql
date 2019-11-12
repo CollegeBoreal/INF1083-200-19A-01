a0dda556b2cb972bbe61f0ab0ffacb34b418a47f
BGBG <43278130+BgbgL13@users.noreply.github.com>
2019-11-12 05:23:53 -0800

    Update 300105201-schema.sql

--- 4.DML/300105201/300105201-schema.sql
+++ 4.DML/300105201/300105201-schema.sql
@@ -7,21 +7,39 @@ GRANT ALL ON musicshop.* TO 'musicshop'@'localhost';
 use musicshop;
 
 
-CREATE TABLE Customers  (
-  initiale VARCHAR(30) NOT NULL
+CREATE TABLE SERVICES_OFFERED  (
+  service_offered INT NOT NULL,
+  Services VARCHAR(30) NOT NULL,
+  PRIMARY KEY(service_offered)
   );
+  
+CREATE TABLE CUSTOMERS  (
+  customer INT AUTO_INCREMENT,
+  Name VARCHAR(30) NOT NULL,
+  Phone_number INT NOT NULL,
+  PRIMARY KEY(customer)
+  );
+  
+   CREATE TABLE INSTRUMENTS (
+  instrument INT AUTO_INCREMENT,
+  Types TEXT NOT NULL,
+  Name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(instrument)
+  );
+  
+ CREATE TABLE COLOURS (
+  colour INT AUTO_INCREMENT,
+  Color VARCHAR(30) NOT NULL,
+   PRIMARY KEY(colour)
+  ); 
 
-CREATE TABLE Price (
-  nom VARCHAR(30) NOT NULL,
-  note INT
+CREATE TABLE PRICES (
+  price  INT AUTO_INCREMENT,
+  instrument INT,
+  colour INT,
+  PRIMARY KEY(price, instrument),
+    FOREIGN KEY(instrument)
+      REFERENCES INSTRUMENTS(instrument),
+    FOREIGN KEY(colour)
+      REFERENCES COLOURS(colour)
   );
-  
-  CREATE TABLE Instruments (
-  nom VARCHAR(30) NOT NULL,
-  note INT
-  );
-  
- CREATE TABLE Color (
-  nom VARCHAR(30) NOT NULL,
-  note INT
-  );-- 
