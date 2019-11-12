2a01b7dcb44bd2a7668300878d9c6d21c6274ff1
BGBG <43278130+BgbgL13@users.noreply.github.com>
2019-11-12 05:17:43 -0800

    Update 300105201-data.sql

--- 4.DML/300105201/300105201-data.sql
+++ 4.DML/300105201/300105201-data.sql
@@ -1,15 +1,19 @@
--- Dans le domaine
+ -- Dans le domaine
 
 use musicshop;
 
 -- Données des tables
 
-INSERT INTO SERVICES_OFFERED (Sale) VALUES ('Keyboard');
-INSERT INTO SERVICES_OFFERED (Sale) VALUES ('Piano');
-INSERT INTO SERVICES_OFFERED (Sale) VALUES ('Module');
+INSERT INTO SERVICES_OFFERED (service_offered, Services) VALUES (1, 'Instruments');
 
-INSERT INTO COLOURS (Colours, model) VALUES ('Blue', 5);
-INSERT INTO COLOURS (Colours, model) VALUES ('Red', 4);
-INSERT INTO COLOURS (Colours, model) VALUES ('White', 4);
+INSERT INTO CUSTOMERS (customer, Name, Phone_number) VALUES (1, John, 6477765643);
 
+INSERT INTO INSTRUMENTS (instrument, Types, Name) VALUES (1, 'Percusion', 'Piano');
+INSERT INTO INSTRUMENTS (instrument, Types, Name) VALUES (2, 'Cordes', 'Guitar');
+
+INSERT INTO COLOURS (Colour, Colors) VALUES (1, 'White');
+INSERT INTO COLOURS (Colour, Colors) VALUES (2, 'Black');
+
+INSERT INTO  PRICES (price, instrument, colour) VALUES (7000, 1, 2);
+INSERT INTO  PRICES (price, instrument, colour) VALUES (5000, 2, 1);
 
