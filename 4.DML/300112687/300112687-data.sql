f46361ee4fe1dcbd5478ad236ec535843e46879c
jthn9022 <43135622+jthn9022@users.noreply.github.com>
2019-11-13 19:44:27 -0500

    Update 300112687-data.sql

--- 4.DML/300112687/300112687-data.sql
+++ 4.DML/300112687/300112687-data.sql
@@ -1,9 +1,8 @@
-
---Dans le Domaine
+-- Dans le Domaine
 
  use Assurance;
 
---Données de la table Assurance 
+-- Données de la table Assurance 
 
 insert into Assurance ( model plan, subscription plan) values (months, type );
 insert into Assurance (model plan , subscription plan) values (months, type );
