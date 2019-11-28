645c62ad365b1924230e5112676de6a0c8a4089e
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-11-28 18:19:29 -0500

    Update 300107361-data.sql

--- 4.DML/300107361/300107361-data.sql
+++ 4.DML/300107361/300107361-data.sql
@@ -40,7 +40,7 @@ INSERT INTO PRICES (price, model) VALUES (1430000, 1);
 
 INSERT INTO CLIENTS (Name, FirstName) VALUES ('Etienne', 'Joky');
 
-INSERT INTO VENTES (client, price, Recu)   VALUES (1, DEFAULT, 'Merci pour votre achat');
+INSERT INTO VENTES (client, price, Recu)   VALUES (1, 1, 'Merci pour votre achat');
 
 INSERT INTO MAINTENANCES (Maintenances) VALUES ('Brake');
 INSERT INTO MAINTENANCES (Maintenances) VALUES ('Repair');
