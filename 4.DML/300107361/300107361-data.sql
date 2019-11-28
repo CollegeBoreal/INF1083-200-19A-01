e008bd02694cdfabaca8713b21f875b21f862003
Etienne Tochgaly <43481043+toch90@users.noreply.github.com>
2019-11-28 17:12:13 -0500

    :fire:

--- 4.DML/300107361/300107361-data.sql
+++ 4.DML/300107361/300107361-data.sql
@@ -16,33 +16,23 @@ INSERT INTO ENGIN_TYPES (Engins) VALUES ('Electric');
 INSERT INTO ENGIN_TYPES (Engins) VALUES ('Hybrid');
 INSERT INTO ENGIN_TYPES (Engins) VALUES ('Petrol');
 
-INSERT INTO MARKS (Marks, origin) VALUES ('Acura', 2);
-INSERT INTO MARKS (Marks, origin) VALUES ('JEEP', 3);
-INSERT INTO MARKS (Marks, origin) VALUES ('Lamborghini', 1);
+INSERT INTO MARKS (Makes) VALUES ('Acura');
+INSERT INTO MARKS (Makes) VALUES ('JEEP');
+INSERT INTO MARKS (Makes) VALUES ('Lamborghini');
 
 INSERT INTO YEARS (Years) VALUES ('2019-02-20');
 INSERT INTO YEARS (Years) VALUES ('2019-01-03');
 INSERT INTO YEARS (Years) VALUES ('2015-11-09');
 
-INSERT INTO MODELS (Models, mark, year) VALUES ('AVENTADOR', 3, 2);
-INSERT INTO MODELS (Models, mark, year) VALUES ('NSX', 1, 3);
-INSERT INTO MODELS (Models, mark, year) VALUES ('WRANGLER', 2, 1);
+INSERT INTO COLOURS (Colours) VALUES ('Black');
+INSERT INTO COLOURS (Colours) VALUES ('BLUEU');
+INSERT INTO COLOURS (Colours) VALUES ('GRAY');
+INSERT INTO COLOURS (Colours) VALUES ('GREEN');
+INSERT INTO COLOURS (Colours) VALUES ('WHITE');
 
-INSERT INTO COLOURS (Colours, model) VALUES ('Black', 1);
-INSERT INTO COLOURS (Colours, model) VALUES ('BLUEU', 1);
-INSERT INTO COLOURS (Colours, model) VALUES ('GRAY', 1);
-INSERT INTO COLOURS (Colours, model) VALUES ('GREEN', 1);
-INSERT INTO COLOURS (Colours, model) VALUES ('WHITE', 1);
-INSERT INTO COLOURS (Colours, model) VALUES ('Black', 2);
-INSERT INTO COLOURS (Colours, model) VALUES ('BLUEU', 2);
-INSERT INTO COLOURS (Colours, model) VALUES ('GRAY', 2);
-INSERT INTO COLOURS (Colours, model) VALUES ('GREEN', 2);
-INSERT INTO COLOURS (Colours, model) VALUES ('WHITE', 2);
-INSERT INTO COLOURS (Colours, model) VALUES ('Black', 3);
-INSERT INTO COLOURS (Colours, model) VALUES ('BLUEU', 3);
-INSERT INTO COLOURS (Colours, model) VALUES ('GRAY', 3);
-INSERT INTO COLOURS (Colours, model) VALUES ('GREEN', 3);
-INSERT INTO COLOURS (Colours, model) VALUES ('WHITE', 3);
+INSERT INTO MODELS (model, Models, make, year, colour, origin) VALUES (1, 'AVENTADOR', 3, 2, [1,2,3,4,5], 1);
+INSERT INTO MODELS (model, Models, make, year, colour, origin) VALUES (2, 'NSX', 1, 3, [1,2,3,4,5], 2);
+INSERT INTO MODELS (model, Models, make, year, colour, origin) VALUES (3, 'WRANGLER', 2, 1, [1,2,3,4,5], 3);
 
 INSERT INTO PRICES (price, model) VALUES (43000, 3);
 INSERT INTO PRICES (price, model) VALUES (39000, 2);
