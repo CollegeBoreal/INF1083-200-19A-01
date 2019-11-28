3d02d8bdbf8ab385b3b3b6540ec0bda280b52449
didier300110500 <guigma_d@yahoo.fr>
2019-11-28 15:14:22 -0500

    c'est encore possible

---
+++ Q.Query/300110500-data.sql
@@ -0,0 +1,13 @@
+-- Dans le domaine
+
+use immigration_consulting;
+
+-- Données des tables
+
+INSERT INTO TYPE_VISAS (Type_visa, Name, Etudiant) VALUES ('VE', 'Hamidou_Zongo', 'BFA');
+INSERT INTO TYPE_VISAS (Type_visa, Name, Resident_permanent) VALUES ('VRP', 'Cristiano_Ronaldo', 'POR');
+INSERT INTO HONORAIRE_PAYMENTS ( Name, Periode, Especes) VALUES ('Reglement', '18-10-2019', '1000$');
+
+INSERT INTO VILLE_RESIDENT (Name Zone_afrique) VALUES ('Hamidou_zongo', 'Congo'); 
+INSERT INTO TYPE_SERVICES (Type_visa, Honoraire_payment, Type _services) VALUES ('10', '1000', '1');
+
