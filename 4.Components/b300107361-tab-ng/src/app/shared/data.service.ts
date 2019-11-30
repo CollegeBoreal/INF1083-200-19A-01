1718ec87384af9da75d2c5f4872e0e4640070ac5
Etienne Tochgaly <etiennetochgaly@gmail.com>
2019-11-29 21:46:27 -0500

    Personal project

--- 4.Components/b300107361-tab-ng/src/app/shared/data.service.ts
+++ 4.Components/b300107361-tab-ng/src/app/shared/data.service.ts
@@ -4,6 +4,7 @@ export interface IDataItem {
     id: number;
     name: string;
     description: string;
+    avatar: string;
 }
 
 @Injectable({
@@ -14,83 +15,69 @@ export class DataService {
     private items = new Array<IDataItem>(
         {
             id: 1,
-            name: "Item 1",
-            description: "Description for Item 1"
+            name: "Acura GoodSon",
+            description: "Description for Item 1",
+            avatar: 'src="src/app/www/car_x/1.png"'
         },
         {
             id: 2,
-            name: "Item 2",
-            description: "Description for Item 2"
+            name: "Acura NSX",
+            description: "Description for Item 2",
+            avatar: ""
         },
         {
             id: 3,
-            name: "Item 3",
-            description: "Description for Item 3"
+            name: "Acura 2020",
+            description: "Description for Item 3",
+            avatar: ""
         },
         {
             id: 4,
-            name: "Item 4",
-            description: "Description for Item 4"
+            name: "Chevrolet Malibu",
+            description: "Description for Item 4",
+            avatar: ""
         },
         {
             id: 5,
-            name: "Item 5",
-            description: "Description for Item 5"
+            name: "Chevrolet Stingray",
+            description: "Description for Item 5",
+            avatar: ""
         },
         {
             id: 6,
-            name: "Item 6",
-            description: "Description for Item 6"
+            name: "Chevrolet 2020",
+            description: "Description for Item 6",
+            avatar: ""
         },
         {
             id: 7,
-            name: "Item 7",
-            description: "Description for Item 7"
+            name: "Corvette",
+            description: "Description for Item 7",
+            avatar: ""
         },
         {
             id: 8,
-            name: "Item 8",
-            description: "Description for Item 8"
+            name: "Honda Passport",
+            description: "Description for Item 8",
+            avatar: ""
         },
         {
             id: 9,
-            name: "Item 9",
-            description: "Description for Item 9"
+            name: "Jaguar iPace",
+            description: "Description for Item 9",
+            avatar: ""
         },
         {
             id: 10,
-            name: "Item 10",
-            description: "Description for Item 10"
+            name: "Jeep Wrangler",
+            description: "Description for Item 10",
+            avatar: ""
         },
         {
             id: 11,
-            name: "Item 11",
-            description: "Description for Item 11"
-        },
-        {
-            id: 12,
-            name: "Item 12",
-            description: "Description for Item 12"
-        },
-        {
-            id: 13,
-            name: "Item 13",
-            description: "Description for Item 13"
-        },
-        {
-            id: 14,
-            name: "Item 14",
-            description: "Description for Item 14"
-        },
-        {
-            id: 15,
-            name: "Item 15",
-            description: "Description for Item 15"
-        },
-        {
-            id: 16,
-            name: "Item 16",
-            description: "Description for Item 16"
+            name: "Lamborghini Aventador",
+            description: "Description for Item 11",
+            avatar: ""
         }
     );
 
