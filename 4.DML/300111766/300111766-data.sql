86ecdea880f80527fec53f9dc776639cefb80496
cheikthiam <44683625+Cheikhthiam@users.noreply.github.com>
2019-12-01 19:24:35 -0800

    cheikh

--- 4.DML/300111766/300111766-data.sql
+++ 4.DML/300111766/300111766-data.sql
@@ -5,13 +5,13 @@ use vente_portable;
 
   -- Données de la table MARKS
 
-INSERT INTO MARKS ( marks ) VALUES ( ‘ihone’ );
-INSERT INTO MARKS ( marks  ) VALUES ( ‘samsung' );
+INSERT INTO MARKS ( marks) VALUES ( ‘ihone’ );
+INSERT INTO MARKS ( marks) VALUES ( ‘samsung' );
 
   -- Données de la table MODELS
 
 INSERT INTO MODELS ( models ) VALUES ( ‘11max’ );
-INSERT INTO MODELS ( nom, Date ) VALUES ('galaxyA50');
+INSERT INTO MODELS ( models,) VALUES ('galaxyA50');
 
   -- Données de la table INVOICES
 
@@ -19,5 +19,5 @@ INSERT INTO INVOICES ( Nom, Date ) VALUES ( ‘Abdel’, 4/10/2019);
 INSERT INTO INVOICES ( Nom, Date ) VALUES ( 'Orden', 1/09/2019);
                                                   
   -- Données de la table PRIX
-INSERT INTO PRIX (mark, model, prix ) VALUES (iphone , 11max, 1200);
-INSERT INTO PRIX (mark, model, prix) VALUES (samsung , galaxyA20, 800) ;
+INSERT INTO PRIX (mark, model, prix ) VALUES ('iphon', '11max' , 1200);
+INSERT INTO PRIX (mark, model, prix) VALUES ('samsung', 'galaxyA20' , 800) ;
