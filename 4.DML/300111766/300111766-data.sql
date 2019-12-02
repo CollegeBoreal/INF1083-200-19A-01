833f74c88e8e09dcac6dbb8814f01ad5ebedea65
cheikthiam <44683625+Cheikhthiam@users.noreply.github.com>
2019-12-01 19:16:48 -0800

    cheikh

--- 4.DML/300111766/300111766-data.sql
+++ 4.DML/300111766/300111766-data.sql
@@ -10,13 +10,13 @@ INSERT INTO MARKS ( marks  ) VALUES ( ‘samsung' );
 
   -- Données de la table MODELS
 
-INSERT INTO PRODUCTS ( models ) VALUES ( ‘11max’ );
-INSERT INTO PRODUCTS ( nom, Date ) VALUES ('galaxyA50');
+INSERT INTO MODELS ( models ) VALUES ( ‘11max’ );
+INSERT INTO MODELS ( nom, Date ) VALUES ('galaxyA50');
 
   -- Données de la table INVOICES
 
 INSERT INTO INVOICES ( Nom, Date ) VALUES ( ‘Abdel’, 4/10/2019);
-INSERT INTO INSTTALATIONS ( Nom, Date ) VALUES ( 'Orden', 1/09/2019);
+INSERT INTO INVOICES ( Nom, Date ) VALUES ( 'Orden', 1/09/2019);
                                                   
   -- Données de la table PRIX
 INSERT INTO PRIX (mark, model, prix ) VALUES (iphone , 11max, 1200);
