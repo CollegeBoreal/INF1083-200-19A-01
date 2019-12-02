b975e5c2625e120f39779dfed81b2602a50b4166
8dbe <43479739+8dbe@users.noreply.github.com>
2019-12-02 07:08:04 -0500

    Update data.service.ts

--- 4.Components/b300113775-tab-ng/src/app/shared/data.service.ts
+++ 4.Components/b300113775-tab-ng/src/app/shared/data.service.ts
@@ -4,6 +4,7 @@ export interface IDataItem {
     id: number;
     name: string;
     description: string;
+    avatar: string;
 }
 
 @Injectable({
@@ -12,106 +13,56 @@ export interface IDataItem {
 export class DataService {
 
     private items = new Array<IDataItem>(
+        
         {
             id: 1,
-            name: "Item 1",
-            description: "Description for Item 1"
+            name: "portrait1",
+            description: "fabrice",
+            avatar: "https://github.com/8dbe/picasa/blob/master/pictures/p1.jpeg"
         },
         {
             id: 2,
-            name: "Item 2",
-            description: "Description for Item 2"
+            name: "portrait2",
+            description: "toch",
+            avatar: "https://github.com/8dbe/picasa/blob/master/pictures/p2.jpeg"
         },
         {
             id: 3,
-            name: "Item 3",
-            description: "Description for Item 3"
+            name: "portrait3",
+            description: "toch",
+            avatar: "https://github.com/8dbe/picasa/blob/master/pictures/p3.jpeg"
         },
         {
             id: 4,
-            name: "Item 4",
-            description: "Description for Item 4"
+            name: "Food",
+            description: "food1",
+            avatar: "https://github.com/8dbe/picasa/blob/master/pictures/p4.jpeg"
         },
         {
             id: 5,
-            name: "Item 5",
-            description: "Description for Item 5"
+            name: "Nature",
+            description: "flowers",
+            avatar: "https://github.com/8dbe/picasa/blob/master/pictures/p5.jpeg"
         },
         {
             id: 6,
-            name: "Item 6",
-            description: "Description for Item 6"
+            name: "Photoshop",
+            description: "edit",
+            avatar: "https://github.com/8dbe/picasa/blob/master/pictures/p6.jpeg"
         },
         {
             id: 7,
-            name: "Item 7",
-            description: "Description for Item 7"
+            name: "Haitian Food",
+            description: "mets haitiens",
+            avatar: "https://github.com/8dbe/picasa/blob/master/pictures/p7.jpeg"
         },
         {
             id: 8,
-            name: "Item 8",
-            description: "Description for Item 8"
-        },
-        {
-            id: 9,
-            name: "Item 9",
-            description: "Description for Item 9"
-        },
-        {
-            id: 10,
-            name: "Item 10",
-            description: "Description for Item 10"
-        },
-        {
-            id: 11,
-            name: "Item 11",
-            description: "Description for Item 11"
-        },
-        {
-            id: 12,
-            name: "Item 12",
-            description: "Description for Item 12"
-        },
-        {
-            id: 13,
-            name: "Item 13",
-            description: "Description for Item 13"
-        },
-        {
-            id: 14,
-            name: "Item 14",
-            description: "Description for Item 14"
-        },
-        {
-            id: 15,
-            name: "Item 15",
-            description: "Description for Item 15"
-        },
-        {
-            id: 16,
-            name: "Item 16",
-            description: "Description for Item 16"
-        },
-        {
-            id: 17,
-            name: "Item 17",
-            description: "Description for Item 17"
-        },
-        {
-            id: 18,
-            name: "Item 18",
-            description: "Description for Item 18"
-        },
-        {
-            id: 19,
-            name: "Item 19",
-            description: "Description for Item 19"
-        },
-        {
-            id: 20,
-            name: "Item 20",
-            description: "Description for Item 20"
+            name: "hiver",
+            description: "Winter In Canada",
+            avatar: "https://github.com/8dbe/picasa/blob/master/pictures/p8.jpeg"
         }
+        
     );
 
     getItems(): Array<IDataItem> {
