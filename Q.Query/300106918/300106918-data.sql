a0274f40bd0983cef0cdc0c1e87ef4da8b01e81d
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2019-12-03 14:47:39 -0500

    Update 300106918-data.sql

--- Q.Query/300106918/300106918-data.sql
+++ Q.Query/300106918/300106918-data.sql
@@ -10,12 +10,24 @@ INSERT INTO SOLUTIONS ( solution, nom  ) VALUES (22, 'configuration');
 INSERT INTO SOLUTIONS ( solution, nom  ) VALUES (34, 'vlan');
 INSERT INTO SOLUTIONS ( solution, nom  ) VALUES (56, 'routeur');
 
--- Données de la table product
+-- Données de la table PRODUITS
 
-INSERT INTO PRODUCTS ( nom, Date ) VALUES ('router', 2020-11-09);
-INSERT INTO PRODUCTS ( nom, Date ) VALUES ('switches', 2014-10-04);
-INSERT INTO PRODUCTS ( nom, Date ) VALUES ('camera', 2014-10-01);
-INSERT INTO PRODUCTS ( nom, Date ) VALUES ('cables', 2014-10-07);
+INSERT INTO PRODUCTS ( IDproduit, nom, date, total_en_stock ) VALUES (10, 'router', '2020-11-09', 233);
+INSERT INTO PRODUCTS ( IDproduit, nom, Date, total_en_stock ) VALUES (20, 'switches','2014-10-04', 130);
+INSERT INTO PRODUCTS ( IDproduit, nom, Date, total_en_stock) VALUES (30, 'camera', '2014-10-01', 144);
+
+-- Données de la table COMMANDES
+
+INSERT INTO COMMANDES (IDproduit,IDclient, Quantit, Date_livraison) VALUES (10, 40, 100, '2020-09-13');
+INSERT INTO COMMANDES (IDproduit,IDclient, Quantit, Date_livraison) VALUES (20, 50, 100, '2020-09-13');
+INSERT INTO COMMANDES (IDproduit,IDclient, Quantit, Date_livraison) VALUES (30, 60, 100, '2020-09-13');
+
+
+-- Données de la table CLIENTS
+
+INSERT INTO CLIENTS ( IDclient, nom, ville) VALUES (40, 'olitan', 'toronto');
+INSERT INTO CLIENTS ( IDclient, nom, ville) VALUES (50, 'aek', 'northyork');
+INSERT INTO CLIENTS (IDclient, nom, ville) VALUES (60, 'jemma', 'mississauga');
 
 
 -- Données de la table insttalation
