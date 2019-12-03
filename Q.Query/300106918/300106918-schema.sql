91a62d8795e438e9b86f9b913249fc69a337625f
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2019-12-03 02:39:27 -0500

    Update 300106918-schema.sql

--- Q.Query/300106918/300106918-schema.sql
+++ Q.Query/300106918/300106918-schema.sql
@@ -15,12 +15,33 @@ use network_IT;
 
 -- Mes Tables
 
-CREATE TABLE SOLUTIONS ( solution INT NOT NULL AUTO_INCREMENT, 
+CREATE TABLE SOLUTIONS ( solution INT NOT NULL AUTO_INCREMENT, date Date,
                        Nom VARCHAR(30) NOT NULL,
                        PRIMARY KEY(solution)
                        );
   
-CREATE TABLE PRODUCTS ( Nom VARCHAR(30) NOT NULL, Date INT);
+CREATE TABLE PRODUCTS ( IDproduit INT AUTO_INCREMENT,
+                       Nom VARCHAR(30) NOT NULL, total_en_stock INT, date Date,
+                      PRIMARY KEY(IDproduit)
+                      );
+
+CREATE TABLE CLIENTS (IDclient INT NOT NULL AUTO_INCREMENT, 
+                      Nom VARCHAR(30) NOT NULL, Ville VARCHAR(30) NOT NULL,
+                      téléphone  INT,
+                      PRIMARY KEY(IDclient)
+                     );
+ 
+ CREATE TABLE COMMANDES ( IDproduit INT,
+                          IDclient INT,
+                          IDfacture INT, 
+                          Quantité INT NOT NULL, Date_livraison Date,
+                         
+            PRIMARY KEY(IDproduit, IDclient),
+            FOREIGN KEY(IDproduit) 
+            REFERENCES PRODUCTS (IDproduit),
+            FOREIGN KEY(IDclient) 
+            REFERENCES CLIENTS (IDclient)
+                  );
                      
 CREATE TABLE INSTALLATIONS (installation INT NOT NULL AUTO_INCREMENT,
                            Nom VARCHAR(30) NOT NULL,
