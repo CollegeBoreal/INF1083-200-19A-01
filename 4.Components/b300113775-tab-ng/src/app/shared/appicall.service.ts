79a7d2ed852d9da26d446fdc0eb0ab2b754ccfc7
8dbe <43479739+8dbe@users.noreply.github.com>
2019-12-04 16:49:41 -0500

    Update appicall.service.ts

--- 4.Components/b300113775-tab-ng/src/app/shared/appicall.service.ts
+++ 4.Components/b300113775-tab-ng/src/app/shared/appicall.service.ts
@@ -1,1 +1,34 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map } from "rxjs/operators";
+import { Observable, throwError } from "rxjs";
+import { Country } from "~/app/shared/country";
 
+@Injectable({
+    providedIn: "root"
+})
+export class ApicallService {
+
+    constructor(private httpClient: HttpClient) {}
+
+    searchCountryByName(name: string): Observable<Array<Country>> {
+        let headers: HttpHeaders = new HttpHeaders();
+        headers = headers.append("Accept", "application/json");
+        headers = headers.append(
+            "X-RapidAPI-Key",
+            "c9e162695cmsh7ed6029b76f07abp1c3076jsn91cd049b082f"
+        );
+
+        return this.httpClient.get(
+            `https://restcountries-v1.p.rapidapi.com/capital/` + name,
+            {headers}
+        ).pipe(
+            map((data: Array<Country>) => {
+                return data;
+            }), catchError((error) => {
+                return throwError("Capital not found!");
+            })
+        );
+    }
+
+}
