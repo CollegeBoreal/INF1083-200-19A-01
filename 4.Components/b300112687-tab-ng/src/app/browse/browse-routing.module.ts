62d08f314e5484abf8cc7c8224d0f1aa4fc2d08d
jthn9022 <43135622+jthn9022@users.noreply.github.com>
2019-12-05 10:34:50 -0500

    Update browse-routing.module.ts

--- 4.Components/b300112687-tab-ng/src/app/browse/browse-routing.module.ts
+++ 4.Components/b300112687-tab-ng/src/app/browse/browse-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
-import { HomeComponent } from "./Home.component";
+import { BrowserComponent } from "./Browser.component";
 
 const routes: Routes = [
-    { path: "default", component: HomeComponent }
+    { path: "default", component: BrowserComponent }
 ];
 
 @NgModule({
     imports: [NativeScriptRouterModule.forChild(routes)],
     exports: [NativeScriptRouterModule]
 })
-export class HomeRoutingModule { }
+export class BrowserRoutingModule { }
