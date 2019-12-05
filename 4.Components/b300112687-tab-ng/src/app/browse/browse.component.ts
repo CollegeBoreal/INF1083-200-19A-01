754259bfe5d0f77030bab927da7f44be0e0adf66
jthn9022 <43135622+jthn9022@users.noreply.github.com>
2019-12-05 10:11:47 -0500

    Update browse.component.ts

--- 4.Components/b300112687-tab-ng/src/app/browse/browse.component.ts
+++ 4.Components/b300112687-tab-ng/src/app/browse/browse.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from "@angular/core";
+// somewhere at top of your component or bootstrap file
+import {registerElement} from "nativescript-angular/element-registry";
+registerElement("exoplayer", () => require("nativescript-exoplayer").Video);
+// documentation: https://docs.nativescript.org/angular/plugins/angular-third-party.html#simple-elements
 
 @Component({
-    selector: "Browse",
-    templateUrl: "./browse.component.html"
+    selector: "Home",
+    templateUrl: "./home.component.html"
 })
-export class BrowseComponent implements OnInit {
+export class HomeComponent implements OnInit {
+
     constructor() {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
-        // Use the "ngOnInit" handler to initialize data for the view.
+        // Init your component properties here.
     }
 }
