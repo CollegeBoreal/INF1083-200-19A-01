a0c603e94984e37537de82285cdc553f47c24db6
Hali-Ma <43480203+halimabzn@users.noreply.github.com>
2019-12-09 14:42:56 -0500

    .

--- 4.Components/b300108234-tab-ng/src/app/shared/data.service.ts
+++ 4.Components/b300108234-tab-ng/src/app/shared/data.service.ts
@@ -28,95 +28,18 @@ export class DataService {
         {
             id: 3,
             name: "Print Hijab",
-            description: "",
-            avatar: "~/car_x/5.png"
+            description: "A wide variety of hijab print options",
+            avatar: "~/3.png"
 
         },
         {
             id: 4,
-            name: "Item 4",
-            description: "Description for Item 4"
+            name: "Under Hijab",
+            description: ""
+            avatar: "~/4.png"
         },
-        {
-            id: 5,
-            name: "Item 5",
-            description: "Description for Item 5"
-        },
-        {
-            id: 6,
-            name: "Item 6",
-            description: "Description for Item 6"
-        },
-        {
-            id: 7,
-            name: "Item 7",
-            description: "Description for Item 7"
-        },
-        {
-            id: 8,
-            name: "Item 8",
-            description: "Description for Item 8"
-        },
-        {
-            id: 9,
-            name: "Item 9",
-            description: "Description for Item 9"
-        },
-        {
-            id: 10,
-            name: "Item 10",
-            description: "Description for Item 10"
-        },
-        {
-            id: 11,
-            name: "Item 11",
-            description: "Description for Item 11"
-        },
-        {
-            id: 12,
-            name: "Item 12",
-            description: "Description for Item 12"
-        },
-        {
-            id: 13,
-            name: "Item 13",
-            description: "Description for Item 13"
-        },
-        {
-            id: 14,
-            name: "Item 14",
-            description: "Description for Item 14"
-        },
-        {
-            id: 15,
-            name: "Item 15",
-            description: "Description for Item 15"
-        },
-        {
-            id: 16,
-            name: "Item 16",
-            description: "Description for Item 16"
-        },
-        {
-            id: 17,
-            name: "Item 17",
-            description: "Description for Item 17"
-        },
-        {
-            id: 18,
-            name: "Item 18",
-            description: "Description for Item 18"
-        },
-        {
-            id: 19,
-            name: "Item 19",
-            description: "Description for Item 19"
-        },
-        {
-            id: 20,
-            name: "Item 20",
-            description: "Description for Item 20"
-        }
+       
+       
     );
 
     getItems(): Array<IDataItem> {
